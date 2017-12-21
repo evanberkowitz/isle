7beb335492b3a4b6be0d4e9d3885863178141edf
Evan Berkowitz <git@evanberkowitz.com>
2017-12-21 10:39:09 +0100

    Vector --> Matrix Shaping Example
    To do (spatial matrix) * (spacetime vector) it's useful to think of the spacetime vector as a matrix with a space index and a time index.  Then, multiply the spatial matrix against the space index, wind up with a matrix with again a space index and a time index, and interpret the result as a spacetime vector.
    
    This, surprisingly, actually works in BLAZE!

--- bindings.cpp
+++ bindings.cpp
@@ -1,11 +1,17 @@
 #include <functional>
 
+#include "math.hpp"
+
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/operators.h>
 #include <pybind11/functional.h>
 
-#include "math.hpp"
+#include <blaze/math/Row.h>
+#include <blaze/math/CustomMatrix.h>
+
+using blaze::DynamicMatrix;
+using blaze::StaticVector;
 
 namespace py = pybind11;
 
@@ -18,8 +24,20 @@ struct Foo {
 struct Bar {
     int operator()(std::function<int()> const &f) const {
 
-        Matrix<double> m{3,3};
+        Matrix<double> m{{3,3},{2,4}};
         std::cout << blaze::det(m) << '\n';
+        
+        // blaze::DynamicVector<double, true> v{1,5};
+        
+        blaze::DynamicVector<double> v{1,2,3,-1,-2,-3};
+        blaze::DynamicVector<double> u{0,0,0,0,0,0};
+        blaze::DynamicMatrix<double> space_matrix{{1,3},{4,7}};
+        blaze::CustomMatrix<double,blaze::unaligned,blaze::unpadded,blaze::rowMajor> vit(&v[0], 2, 3);
+        blaze::CustomMatrix<double,blaze::unaligned,blaze::unpadded,blaze::rowMajor> ujs(&u[0], 2, 3);
+        ujs = space_matrix * vit;
+        
+        std::cout << ujs << '\n';
+        std::cout << u << std::endl;
         
         std::cout << f() << '\n';
         return f();
