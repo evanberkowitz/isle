f8c2715faf84f473678aaa071606a9523689c7ba
Christopher Koerber <christopher@koerber-bochum.de>
2017-12-22 15:51:22 +0100

    Added unittests for vector classes

--- unittest/baseTest.py
+++ unittest/baseTest.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 """
-Example python module for unittests.
+Base implementation of unittests
 """
-import unittest
 try:
   import cns
 except ModuleNotFoundError as e:
@@ -11,6 +10,7 @@ except ModuleNotFoundError as e:
 
 import numpy as np
 import operator
+import abc
 
 #===============================================================================
 #     logging
@@ -29,13 +29,6 @@ _ch.setLevel(logging.INFO)
 _ch.setFormatter(_format)
 
 logger.addHandler(_ch)
-#-------------------------------
-
-sources = [el for el in os.listdir() if ".so" in el]
-
-#-------------------------------
-
-version = str(sub.check_output(["git", "rev-parse", "HEAD"]).strip())
 
 
 #===============================================================================
@@ -46,20 +39,23 @@ class RandomInitializer(object):
   def __init__(self):
     "Initialize the type map"
     self.type_map = {
-      "int"    : lambda **kwargs : self._init_scalar(int,       **kwargs),
-      "float"  : lambda **kwargs : self._init_scalar(float,     **kwargs),
-      "double" : lambda **kwargs : self._init_scalar(float,     **kwargs),
-      "DVector": lambda **kwargs : self._init_cvec(cns.DVector, **kwargs),
+      "int"      : lambda **kwargs : self._init_scalar(int,       **kwargs),
+      "float"    : lambda **kwargs : self._init_scalar(float,     **kwargs),
+      "double"   : lambda **kwargs : self._init_scalar(float,     **kwargs),
+      "DVector"  : lambda **kwargs : self._init_cvec(cns.DVector, dtype=float, **kwargs),
+      "IVector"  : lambda **kwargs : self._init_cvec(cns.IVector, dtype=int, **kwargs),
+      cns.DVector: lambda **kwargs : self._init_cvec(cns.DVector, dtype=float, **kwargs),
+      cns.IVector: lambda **kwargs : self._init_cvec(cns.IVector, dtype=int, **kwargs),
     }
 
   #----- Initialize a scalar -----
   def _init_scalar(self, typefunc, **kwargs):
     "Initializes a random scalar of type `dtype`"
-    val = np.random.uniform()*10
+    val = np.random.uniform()*10+1
     return typefunc(val), typefunc(val)
 
   #----- init_cvec -----
-  def _init_cvec(self, cvecType, NVec=None):
+  def _init_cvec(self, cvecType, NVec=None, dtype=float):
     """
     Randomly sets all compenents of a vector cvecType.
     Returns the CVec and the NumpyVec.
@@ -68,7 +64,7 @@ class RandomInitializer(object):
     if NVec is None:
       NVec = int(np.random.uniform()*10+1)
     # create vectors
-    pyvec = np.random.uniform(size=NVec)
+    pyvec = np.array(np.random.uniform(size=NVec)*5-10, dtype=dtype)
     cvec  = cvecType(NVec)
     # Set cvec elements
     for nel, el in enumerate(pyvec):
@@ -77,6 +73,10 @@ class RandomInitializer(object):
 
   #----- Wrapper -----
   def initialize(self, dtype, **kwargs):
+    """
+    Wrapper for initializing objects.
+    Always returns c++ and python object for comparism.
+    """
     out = self.type_map.get(dtype)(**kwargs)
     if out is None:
       raise TypeError("Do not know how to initialize: '{t}'".format(t=dtype))
@@ -89,20 +89,21 @@ class Equalizer(object):
     "If iterable compares element wise, if not direct equal or fail"
     i1 = hasattr(o1 ,"__iter__")
     i2 = hasattr(o2 ,"__iter__")
-    if i1 and i2:
-      if len(o1) == len(o2):
-        for el1, el2 in zip(o1, o2):
+    # test if both iterable
+    if i1 and i2: # both iterable
+      if len(o1) == len(o2): # same length
+        for el1, el2 in zip(o1, o2): # element wise checks
           if abs(el1 - el2) > numPrec:
             return False
         return True
-      else:
+      else: # not same length
         return False
-    elif not(i1) and not(i2):
+    elif not(i1) and not(i2): # both not iterable
       return abs(o1 - o2) < numPrec
-    else:
+    else: # one iterable not iterable
       return False
 
-
+# Easy way to acces operators as functions
 OperatorDict = {
   '+'  : operator.add,
   '+=' : operator.iadd,
@@ -121,39 +122,18 @@ EQ = Equalizer()
 #===============================================================================
 #     Tests
 #===============================================================================
-class TestDVector(unittest.TestCase):
-  "DVector test class"
-  #----- Class Properties -----
-  cvecType   = cns.DVector
-  operations = {
-    "*" : [
-      # input types            output types
-      (("DVector", "DVector"), cns.DVector),
-      (("DVector", "int"    ), cns.DVector),
-      (("DVector", "double" ), cns.DVector),
-      (("DVector", "double" ), cns.DVector),
-      (("int"    , "DVector"), cns.DVector),
-      (("double" , "DVector"), cns.DVector),
-      (("double" , "DVector"), cns.DVector),
-    ],
-    "/" : [
-      (("DVector", "int"),    cns.DVector),
-      (("DVector", "double"), cns.DVector),
-    ],
-    "+" : [
-      (("DVector", "DVector"), cns.DVector),
-    ],
-    "-" : [
-      (("DVector", "DVector"), cns.DVector),
-    ],
-    "@" : [
-      (("DVector", "DVector"), float),
-    ],
-  }
+class AbstractVectorTest(metaclass=abc.ABCMeta):
+  "Abstract vector class test. Vector test must inherit from this class"
+  # C++ base type
+  cvecType   = None
+  # to be checked operator overloads
+  # { operation: ( (InputType1, InputType2), outPutClas ), ...}
+  operations = {}
 
   #----------init test ------------------
   def test_0_init_vec(self):
     "Test if constructor of vector works"
+    logger.info("Testing initialization of {vecType}".format(vecType=self.cvecType))
     NVec = int(np.random.uniform()*10+1)
     vec = self.cvecType(NVec)
     self.assertEqual(NVec, len(vec))
@@ -161,22 +141,24 @@ class TestDVector(unittest.TestCase):
   #----------set test ------------------
   def test_1_set_and_get(self):
     "Test setter and getter of components work"
-    NVec = int(np.random.uniform()*10+1)
-    n1   = int(np.random.uniform()*10)
-    v1   = np.random.uniform()
-    vec  = self.cvecType(NVec)
-    # set random component
-    vec[n1] = v1
-    self.assertTrue( abs(vec[n1]-v1) < 1.e-7 )
+    logger.info("Testing setter of {vecType}".format(vecType=self.cvecType))
+    cOut, pyOut = RI.initialize(self.cvecType)
+    try:
+      self.assertTrue(EQ.isEqual(cOut, pyOut))
+    except AssertionError as e:
+      logger.info("Equality check failed for cVec = pyVec")
+      logger.info("cOut  = {cOut}".format(cOut=cOut))
+      logger.info("pyOut = {pyOut}".format(pyOut=pyOut))
+      raise e
 
   #----------failing test ------------------
   def test_2_test_operators(self):
     "Test the operator overloads"
     NVec = int(np.random.uniform()*10+1)
-    print("\n")
+    # iterate operator types: "+", "-", ...
     for opType, operations in self.operations.items():
+      # iterate input types: ("vec", "vec"), ("vec", "double"), ...
       for (inTypes, outInstance) in operations:
-        
         logger.info(
           "Testing: {cIn1} {op} {cIn2} = {cOut}".format(
             cIn1=inTypes[0], op=opType, cIn2=inTypes[1], cOut=outInstance
@@ -186,33 +168,28 @@ class TestDVector(unittest.TestCase):
         cIn2, pyIn2 = RI.initialize(inTypes[1], NVec=NVec)
         cOut  = OperatorDict[opType](cIn1,  cIn2 )
         pyOut = OperatorDict[opType](pyIn1, pyIn2)
+        # Type check
         try:
           self.assertTrue(isinstance(cOut, outInstance))
         except AssertionError as e:
-          print("\n")
           logger.info(
             "Type check failed for {cIn1} {op} {cIn2} = {cOut}".format(
               cIn1=inTypes[0], op=opType, cIn2=inTypes[1], cOut=outInstance
             )
           )
           raise e
+        # Value check
         try:
           self.assertTrue(EQ.isEqual(cOut, pyOut))
         except AssertionError as e:
-          print("\n")
           logger.info(
             "Equality check failed for {cIn1} {op} {cIn2} = {cOut}".format(
               cIn1=inTypes[0], op=opType, cIn2=inTypes[1], cOut=outInstance
             )
           )
+          logger.info("cIn1 = {cIn1}".format(cIn1=cIn1))
+          logger.info("cIn2 = {cIn2}".format(cIn2=cIn2))
+          logger.info("cOut = {cOut}".format(cOut=cOut))
+          logger.info("pyOut = {pyOut}".format(pyOut=pyOut))
           raise e
 
-#===============================================================================
-#     Exe
-#===============================================================================
-if __name__ == "__main__":
-  logger.info("Sources = " + ", ".join(sources) )
-  logger.info("Version = " + version )
-  logger.info("Starting unittests.\n" )
-
-  unittest.main()
---
+++ unittest/vectorTests.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+"""
+Example python module for unittests.
+"""
+import unittest
+import baseTest
+try:
+  import cns
+except ModuleNotFoundError as e:
+  print("[WARNING] Please copy the build `cns` module in this directory before running the tests...")
+  raise e
+
+#===============================================================================
+#     logging
+#===============================================================================
+import os
+import subprocess as sub
+import logging
+logger = logging.getLogger("baseTest.py")
+#-------------------------------
+sources = [el for el in os.listdir() if ".so" in el]
+version = str(sub.check_output(["git", "rev-parse", "HEAD"]).strip())
+#-------------------------------
+
+
+#===============================================================================
+#     Unit Tests
+#===============================================================================
+class TestDVector(baseTest.AbstractVectorTest, unittest.TestCase):
+  "DVector test class"
+  # C++ base type
+  cvecType   = cns.DVector
+  # to be checked operator overloads
+  # { operation: ( (InputType1, InputType2), outPutClas ), ...}
+  operations = {
+    "*" : [
+     
+      (("DVector", "DVector"), cns.DVector),
+      (("DVector", "int"    ), cns.DVector),
+      (("DVector", "double" ), cns.DVector),
+      (("int"    , "DVector"), cns.DVector),
+      (("double" , "DVector"), cns.DVector),
+    ],
+    "/" : [
+      (("DVector", "int"),    cns.DVector),
+      (("DVector", "double"), cns.DVector),
+    ],
+    "+" : [
+      (("DVector", "DVector"), cns.DVector),
+    ],
+    "-" : [
+      (("DVector", "DVector"), cns.DVector),
+    ],
+    "@" : [
+      (("DVector", "DVector"), float),
+    ],
+  }
+
+
+#-------------------------------
+class TestIVector(baseTest.AbstractVectorTest, unittest.TestCase):
+  "DVector test class"
+  # C++ base type
+  cvecType   = cns.IVector
+  # to be checked operator overloads
+  # { operation: ( (InputType1, InputType2), outPutClas ), ...}
+  operations = {
+    "*" : [
+      # input types            output types
+      (("IVector", "IVector"), cns.IVector),
+      (("IVector", "int"    ), cns.IVector),
+      (("IVector", "double" ), cns.DVector),
+      (("int"    , "IVector"), cns.IVector),
+      (("double" , "IVector"), cns.DVector),
+    ],
+   # "/" : [
+   #   (("IVector", "int"),    cns.IVector),
+   #   (("IVector", "double"), cns.DVector),
+   # ],
+    "+" : [
+      (("IVector", "IVector"), cns.IVector),
+    ],
+    "-" : [
+      (("IVector", "IVector"), cns.IVector),
+    ],
+    "@" : [
+      (("IVector", "IVector"), int),
+    ],
+  }
+
+#===============================================================================
+#     Exe
+#===============================================================================
+if __name__ == "__main__":
+  logger.info("Sources = " + ", ".join(sources) )
+  logger.info("Version = " + version )
+  logger.info("Starting unittests.\n" )
+  unittest.main()
