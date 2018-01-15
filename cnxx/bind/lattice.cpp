84f0408eed25318b6227a2ca562349aa1b874dcf
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-01-15 20:06:17 +0100

    Add getNeighbors function for Python Lattice

--- cnxx/bind/lattice.cpp
+++ cnxx/bind/lattice.cpp
@@ -3,7 +3,9 @@
 #include "../lattice.hpp"
 
 namespace bind {
+
     void bindLattice(py::module &mod) {
+        
         py::class_<Lattice>{mod, "Lattice"}
             .def(py::init<std::size_t, std::size_t>())
             .def("areNeighbors", &Lattice::areNeighbors)
@@ -14,7 +16,10 @@ namespace bind {
                          return self.hopping()(i, j);
                      else
                          throw std::invalid_argument("No matrix element at given indices");
-                })
+                 })
+            .def("getNeighbors", [](const Lattice &self, const std::size_t i) {
+                    return makeIndexValueIterator(self.hopping().begin(i), self.hopping().end(i));
+                }, py::keep_alive<0, 1>())
             .def("distance", (double (Lattice::*)(std::size_t, std::size_t) const)&Lattice::distance)
             .def("distance", (void (Lattice::*)(std::size_t, std::size_t, double))&Lattice::distance)
             .def("nt", &Lattice::nt)
