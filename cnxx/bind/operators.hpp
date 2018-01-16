c6dadc4b0fd141f90a3fbd68bb587cae37a5f045
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-01-16 14:03:47 +0100

    Add add, sub, mul operator metafunctions

---
+++ cnxx/bind/operators.hpp
@@ -0,0 +1,52 @@
+#ifndef OPERATORS_HPP
+#define OPERATORS_HPP
+
+#include <utility>
+
+namespace bind {
+    /// Metafunctions and names for arithmetic operators.
+    namespace op {
+
+        /// Addition operator.
+        struct add {
+            template <typename LHS, typename RHS>
+            static auto f(LHS &&lhs, RHS &&rhs)
+                -> decltype(std::forward<LHS>(lhs) + std::forward<RHS>(rhs)) {
+                return lhs + rhs;
+            }
+        };
+
+        /// Subtraction oeprator.
+        struct sub {
+            template <typename LHS, typename RHS>
+            static auto f(LHS &&lhs, RHS &&rhs)
+                -> decltype(std::forward<LHS>(lhs) - std::forward<RHS>(rhs)) {
+                return lhs - rhs;
+            }
+        };
+
+        /// Multiplication operator.
+        struct mul {
+            template <typename LHS, typename RHS>
+            static auto f(LHS &&lhs, RHS &&rhs)
+                -> decltype(std::forward<LHS>(lhs) * std::forward<RHS>(rhs)) {
+                return lhs * rhs;
+            }
+        };
+
+        /// Python name for operators.
+        template <typename OP>
+        constexpr char name[] = "_";
+
+        template <>
+        constexpr char name<add>[] = "__add__";
+
+        template <>
+        constexpr char name<sub>[] = "__sub__";
+
+        template <>
+        constexpr char name<mul>[] = "__mul__";
+    }
+}
+
+#endif  // ndef OPERATORS_HPP
