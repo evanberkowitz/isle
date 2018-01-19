3f66a0c3dcef02ef2ed5c48aaef3c3de762723a3
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-01-19 10:02:02 +0100

    Remove test stuff from modules dir

--- modules/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import yaml
-import cns
-
-with open("c60_ipr.yml", "r") as yamlf:
-    lat = yaml.safe_load(yamlf)
-with open("out.yml", "w") as yamlf:
-    yaml.dump(lat, yamlf)
