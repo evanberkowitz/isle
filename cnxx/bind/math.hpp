1c84bb826cc173bb82e76e2ec24da89c5c443a22
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-02-02 15:13:31 +0100

    Remove row() for SymmetricSparsematrix

--- cnxx/bind/math.hpp
+++ cnxx/bind/math.hpp
@@ -10,6 +10,14 @@
 
 namespace bind {
     /// Bind Vector, Matrix, and SparseMatrix.
+    /**
+     * \bug Buffer construction from numpy array of ints not possible.
+     *      Format descriptor for int does not match between Python and C++.
+     *      Looks like a bug in Pybind11. Descriptors are
+     *      - numpy array of int: `'l'`
+     *      - C++ int: `'i'`
+     *      - C++ long: `'q'`
+     */
     void bindTensors(py::module &mod);
 }
 
