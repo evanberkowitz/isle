a66bcf948e93c2ca82624f8e19734961f1756211
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-02-26 10:20:18 +0100

    cns wrappers around tensors can now deduce dtype

--- modules/cns/cnxx_wrappers.py
+++ modules/cns/cnxx_wrappers.py
@@ -3,8 +3,9 @@ Imports everything from cnxx into this modules namespace and defines wrappers
 around linear algebra types.
 """
 
-from cns.core import prepare_cnxx_import
+import numpy as np
 
+from cns.core import prepare_cnxx_import
 prepare_cnxx_import()
 from cnxx import *
 
@@ -17,13 +18,22 @@ class Vector:
     on the argument dtype.
     """
 
-    def __new__(cls, *args, dtype=float, **kwargs):
-        "Create and return a Vector for given datatype."
-        if dtype == float:
+    def __new__(cls, *args, dtype=None, **kwargs):
+        """
+        Create and return a Vector for given datatype.
+        `dtype` is deduced from other arguments if possible.
+        """
+
+        # deduce dtype from numpy array
+        if dtype is None and len(args) == 1 and isinstance(args[0], np.ndarray):
+            dtype = args[0].dtype
+
+        # include possible numpy types in checks
+        if np.issubdtype(dtype, np.floating):
             return DVector(*args, **kwargs)
-        if dtype == int:
+        if np.issubdtype(dtype, np.signedinteger):
             return IVector(*args, **kwargs)
-        if dtype == complex:
+        if np.issubdtype(dtype, np.complexfloating):
             return CDVector(*args, **kwargs)
         raise ValueError("Datatype not supported: {}".format(dtype))
 
@@ -35,13 +45,30 @@ class Matrix:
     on the argument dtype.
     """
 
-    def __new__(cls, *args, dtype=float, **kwargs):
-        "Create and return a Matrix for given datatype."
-        if dtype == float:
+    def __new__(cls, *args, dtype=None, **kwargs):
+        """
+        Create and return a Matrix for given datatype.
+        `dtype` is deduced from other arguments if possible.
+        """
+
+        if dtype is None and len(args) == 1:
+            # deduce dtype from numpy array
+            if isinstance(args[0], np.ndarray):
+                dtype = args[0].dtype
+            # or deduce from other cns matrix
+            elif isinstance(args[0], DSparseMatrix) or isinstance(args[0], DMatrix):
+                dtype = float
+            elif isinstance(args[0], ISparseMatrix) or isinstance(args[0], IMatrix):
+                dtype = int
+            elif isinstance(args[0], CDSparseMatrix) or isinstance(args[0], CDMatrix):
+                dtype = complex
+
+        # include possible numpy types in checks
+        if np.issubdtype(dtype, np.floating):
             return DMatrix(*args, **kwargs)
-        if dtype == int:
+        if np.issubdtype(dtype, np.signedinteger):
             return IMatrix(*args, **kwargs)
-        if dtype == complex:
+        if np.issubdtype(dtype, np.complexfloating):
             return CDMatrix(*args, **kwargs)
         raise ValueError("Datatype not supported: {}".format(dtype))
 
@@ -53,8 +80,21 @@ class SparseMatrix:
     on the argument dtype.
     """
 
-    def __new__(cls, *args, dtype=float, **kwargs):
-        "Create and return a SparseMatrix for given datatype."
+    def __new__(cls, *args, dtype=None, **kwargs):
+        """
+        Create and return a SparseMatrix for given datatype.
+        `dtype` is deduced from other arguments if possible.
+        """
+
+        # deduce dtype from other cns matrix
+        if dtype is None and len(args) == 1:
+            if isinstance(args[0], DSparseMatrix) or isinstance(args[0], DMatrix):
+                dtype = float
+            elif isinstance(args[0], ISparseMatrix) or isinstance(args[0], IMatrix):
+                dtype = int
+            elif isinstance(args[0], CDSparseMatrix) or isinstance(args[0], CDMatrix):
+                dtype = complex
+
         if dtype == float:
             return DSparseMatrix(*args, **kwargs)
         if dtype == int:
