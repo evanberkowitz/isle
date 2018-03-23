dcb59e583ee2eb4abacaef9e4ef653d627c65bda
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-03-23 16:35:27 +0100

    New and improved hmc + measurements script

--- scripts/ergodicity.py
+++ scripts/ergodicity.py
@@ -12,8 +12,8 @@ import cns
 
 LATFILE = "two_sites.yml"
 
-NT = 32   # number of time slices
-NTR = 100  # number of trajectories
+NT = 16   # number of time slices
+NTR = 1000  # number of trajectories
 NMD = 3  # number of MD steps per trajectory
 MDSTEP = 1/NMD  # size of MD steps
 
@@ -22,6 +22,71 @@ BETA = 3
 SIGMA_KAPPA = 1
 
 UTILDE = U*BETA/NT
+
+class PhaseMeas:
+    "Record and plot pahses of action."
+
+    def __init__(self):
+        self.phases = []
+
+    def __call__(self, cfg, h):
+        self.phases.append(np.imag(h))
+
+    def report(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.set_title("Phases")
+        ax.plot(self.phases)
+        plt.draw()
+
+class DetMeas:
+    def __init__(self, hfm):
+        self._hfm = hfm
+        self.detM = []
+        self.detQ = []
+
+    def __call__(self, cfg, h):
+        self._hfm.updatePhi(cfg)
+        self.detM.append(np.exp(cns.logdet(cns.Matrix(self._hfm.M(False)))))
+        self.detQ.append(np.exp(cns.logdet(cns.Matrix(self._hfm.Q()))))
+
+    def report(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.set_title("det(M)")
+        plt.hist2d(np.real(self.detM), np.imag(self.detM), bins=40, norm=LogNorm())
+        plt.draw()
+
+
+# TODO how do repro check with generic proposer?
+# TODO I don't want to have to pass ham in, compute hold using only the change in pi
+def hmc(phi, ham, proposer, ntr, measurements, realCheckFreq=0):
+    nacc = 0  # number of accepted trajectories
+    for i in range(ntr):
+        pi = cns.Vector(np.random.normal(0, 1, len(phi))+0j)
+        hold = ham.eval(phi, pi)
+
+        # do MD
+        newPhi, newPi, hnew = proposer(phi, pi)
+
+        # check that phi and pi are real
+        if realCheckFreq != 0 and i % realCheckFreq == 0:
+            if np.max(cns.imag(newPhi))/np.max(cns.real(newPhi)) > 1e-14:
+                raise RuntimeError("phi has acquired an imaginary part at iteration {}: {}".format(i, newPhi))
+            if np.max(cns.imag(newPi))/np.max(cns.real(newPi)) > 1e-14:
+                raise RuntimeError("pi has acquired an imaginary part at iteration {}: {}".format(i, newPi))
+
+        # accept-reject
+        if np.exp(cns.real(hold-hnew)) > np.random.uniform(0, 1):
+            phi = newPhi
+            hold = hnew
+            nacc += 1
+            
+        # perform measurements
+        for meas in measurements:
+            meas(phi, hold)
+
+    return phi, nacc
 
 def main():
     np.random.seed(1)
@@ -36,65 +101,22 @@ def main():
     # initial state
     phi = cns.Vector(np.random.normal(0, np.sqrt(UTILDE), lat.nx()*NT)+0j)
 
-    # TODO store intitial traj?
-    cfgs = []
-    phases = []
+    # thermalize
+    phi, nacct = hmc(phi, ham, lambda phi, pi: cns.leapfrog(phi, pi, ham, 1, NMD),
+                     100, [], realCheckFreq=1)
+    print("Acceptance rate during thermalization: {}".format(nacct/100))
 
-    nacc = 0
-    oldS = None
-    for i in range(NTR):
-        pi = cns.Vector(np.random.normal(0, np.sqrt(UTILDE), len(phi))+0j)
-        if oldS is None:  # have never evaluated an action before, do it now
-            oldS = ham.eval(phi, pi)
+    # define measurements
+    phaseMeas = PhaseMeas()
+    detMeas = DetMeas(cns.HubbardFermiMatrix(kappa, phi, 0, SIGMA_KAPPA))
 
-        # do MD
-        newPhi, newPi, newS = cns.leapfrog(phi, pi, ham, 1, NMD)
+    # measure
+    phi, nacct = hmc(phi, ham, lambda phi, pi: cns.leapfrog(phi, pi, ham, 1, NMD),
+                     NTR, [phaseMeas, detMeas], realCheckFreq=1)
+    print("Acceptance rate during measurements: {}".format(nacct/NTR))
 
-        # reproducibility check
-        # repPhi, repPi, _ = cns.leapfrog(newPhi, newPi, ham, 1, NMD, -1)
-        # if np.linalg.norm(repPhi-phi) > 1e-10:
-        #     print("Repro check failed in traj {} with error in phi: {}".format(i, np.linalg.norm(repPhi-phi)))
-        #     return
-        # if np.linalg.norm(repPi-pi) > 1e-10:
-        #     print("Repro check failed in traj {} with error in pi: {}".format(i, np.linalg.norm(repPi-pi)))
-        #     return
-
-        # check for imaginary parts
-        if np.max(cns.imag(newPhi)) > 1e-14:
-            print("phi has acquired an imaginary part: {}".format(newPhi))
-            return
-        if np.max(cns.imag(newPi)) > 1e-14:
-            print("pi has acquired an imaginary part: {}".format(newPhi))
-            return
-
-        if np.exp(cns.real(oldS-newS)) > np.random.uniform(0, 1):
-            print("accept: ", newS-oldS)
-            oldS = newS
-            phi = newPhi
-            nacc += 1
-        else:
-            print("reject: ", newS-oldS)
-
-        cfgs.append(phi)
-        phases.append(cns.imag(newS))
-
-
-    print("max phase: ", np.max(np.abs(phases)))
-
-    print("acceptance rate: ", nacc/NTR)
-
-    dets= list(map(
-        lambda cfg:
-        np.exp(cns.logdet(cns.Matrix(
-            cns.HubbardFermiMatrix(kappa, cfg, 0, SIGMA_KAPPA).M(False))
-        )), cfgs))
-    detsReal, detsImag = np.array(dets).real, np.array(dets).imag
-
-    plt.figure()
-    plt.title(r"$\mathrm{det}(M)$")
-#    plt.hist(detsReal, bins=32)
-#    plt.hist2d(detsReal, detsImag, [-20.+ i*40./40 for i in range(40)], norm=LogNorm())
-    plt.hist2d(detsReal, detsImag, bins=40, norm=LogNorm())
+    phaseMeas.report()
+    detMeas.report()
     plt.show()
 
 if __name__ == "__main__":
