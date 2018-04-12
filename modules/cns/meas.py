710909b6bb1d68c49e916b20486ee504c678f932
Thomas Luu <--global>
2018-04-12 16:05:45 +0200

    added measurement class Phi

--- modules/cns/meas.py
+++ modules/cns/meas.py
@@ -20,6 +20,63 @@ def _newAxes(title, xlabel, ylabel):
     return fig, ax
 
 
+class Phi:
+    r"""!
+    \ingroup meas
+    Tabulate phi and mean value of phi^2.
+    """
+
+    def __init__(self):
+        self.phi = []
+        self.phiSq = []
+
+    def __call__(self, itr, phi, act, acc):
+        """!Record phi and mean value of phi^2 rate."""
+        phi1 = 0.
+        phiSq = 0.
+        for i in range(len(phi)):
+            phi1 += phi[i]
+            phiSq += np.real(phi[i]*np.conj(phi[i]))
+        self.phi.append(np.real(phi1))
+        self.phiSq.append(phiSq/len(phi))
+
+    def reportPhiSq(self, binsize, ax=None, fmt=""):
+        r"""!
+        Plot the <phi^2> against Monte Carlo time.
+        \param binsize The acceptance rate is averaged over `binsize` trajectories.
+        \param ax Matplotlib Axes to plot in. If `None`, a new one is created in a new figure.
+        \param fmt Plot format passed to matplotlib. Can encode color, marker and line styles.
+        \returns The Axes with the plot.
+        """
+
+        binned = binnedArray(self.phiSq, binsize)
+
+        # make a new axes is needed
+        doTightLayout = False
+        if ax is None:
+            fig, ax = _newAxes(r"global mean of <$\phi^2$> = {:3.2f}".format(np.mean(binned)),
+                               r"$N_{\mathrm{tr}}$", r"<$\phi^2$>($N_{\mathrm{tr}})$")
+            doTightLayout = True
+
+        # plot <phi^2>
+        ax.plot(np.arange(0, len(self.phiSq), binsize), binned,
+                fmt, label=r"<$\phi^2$>($N_{\mathrm{tr}})$")
+        ax.set_ylim(ymin = 0)
+        if doTightLayout:
+            fig.tight_layout()
+
+        return ax
+
+    def reportPhi(self, ax=None, fmt=""):
+        r"""!
+        Plot histogram of summed phi.
+        \param ax Matplotlib Axes to plot in. If `None`, a new one is created in a new figure.
+        \param fmt Plot format passed to matplotlib. Can encode color, marker and line styles.
+        \returns The Axes with the plot.
+        """
+        # here I want to plot a histogram of Phi
+
+
 class AcceptanceRate:
     r"""!
     \ingroup meas
