f7e67d74e5a4589d811ddf4b9b12b69fb80a674b
Evan Berkowitz <git@evanberkowitz.com>
2018-04-13 16:27:06 +0200

    Make pylint happy about progress measurement.

--- modules/cns/meas/progress.py
+++ modules/cns/meas/progress.py
@@ -14,26 +14,27 @@ class Progress:
         r"""!
         Create a Progress report.
         \param stepName A string that indicates what kind of progress is being made.
-        \param targetIteration When the iteration count reaches `targetIteration` the computation should be complete.
+        \param targetIteration When the iteration count reaches `targetIteration`
+               the computation should be complete.
         \param startIteration  Sets 0% complete to be at `startIteration`
         """
-        self.name   = stepName
-        self.start  = startIteration
-        self.current= self.start
-        self.percent= 0
+        self.name = stepName
+        self.start = startIteration
+        self.current = self.start
+        self.percent = 0
         self.target = targetIteration
-        digits=str(int(np.ceil(np.log10(self.target))))
+        digits = str(int(np.ceil(np.log10(self.target))))
         self.string = "{} {:5.2f}% complete at iteration {:"+digits+"} (aiming for {:"+digits+"})."
 
     def __call__(self, phi, inline=True, **kwargs):
         """!Print progress report."""
-        if(inline):
+        if inline:
             self.current = kwargs['itr']
             self.percent = 100 * (self.current-self.start) / (self.target-self.start)
             print(self.string.format(self.name, self.percent, self.current, self.target))
         else:
             pass
 
-    def report():
+    def report(self):
         """!Print most recent progress report."""
         print(self.string.format(self.name, self.percent, self.current, self.target))
