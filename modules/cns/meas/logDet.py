d545c0c2b54aea819af8cce2c6db1bc649c4002b
Evan Berkowitz <git@evanberkowitz.com>
2018-04-13 17:37:49 +0200

    First pass at adding logDet measurement

---
+++ modules/cns/meas/logDet.py
@@ -0,0 +1,47 @@
+"""!
+Measurement of logDet.
+"""
+
+import numpy as np
+import cns
+from matplotlib.colors import LogNorm
+
+from .common import newAxes
+
+class LogDet:
+    r"""!
+    \ingroup meas
+    Measure the log determinant of particles and holes.
+    """
+
+    def __init__(self, kappaTilde, mu, SIGMA_KAPPA):
+        self.M = cns.HubbardFermiMatrix(kappaTilde, mu, SIGMA_KAPPA)
+        self.logDet = {cns.PH.PARTICLE: [], cns.PH.HOLE: []}
+
+    def __call__(self, phi, inline=False, **kwargs):
+        """!Record logDet."""
+        for species in self.logDet:
+            self.logDet[species].append(cns.logdetM(self.M, phi, cns.PH.PARTICLE))
+
+    def report(self, species=cns.PH.PARTICLE, binsize=41, ax=None):
+        r"""!
+        Plot the determinant in the complex plane
+        \param species a `cns.PH` species that identifies the determinant of interest.
+        \param binzise Number of bins in each direction.
+                       An odd number highlights the avoidance of 0 determinant.
+        \param ax Matplotlib Axes to plot in. If `None`, a new one is created in a new figure.
+        \returns The Axes with the plot.
+        """
+
+        if ax is None:
+            fig, ax = newAxes(str(species)+" Determinant", r"Re($det$)", r"Im($det$)")
+            doTightLayout = True
+
+        dets = np.exp(self.logDet[species])
+
+        ax.hist2d(np.real(dets), np.imag(dets), bins=binsize, norm=LogNorm())
+        ax.set_aspect('equal')
+
+        if doTightLayout:
+            fig.tight_layout()
+        return ax
--- scripts/ergodicity.py
+++ scripts/ergodicity.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """!
 Investigate the ergodicity problem.
 """
@@ -15,7 +16,10 @@ LATFILE = "two_sites.yml"  # input lattice
 
 NT = 8  # number of time slices
 NTHERM = 3000  # number of thermalization trajectories
-NPROD = 10000  # number of production trajectories
+NPROD = 10000 # number of production trajectories
+
+N_LEAPFROG_THERM = 8
+N_LEAPFROG = 3
 
 # model parameters
 U = 2
@@ -46,19 +50,29 @@ def main():
     actMeas = cns.meas.Action()
     thermalizationProgress = cns.meas.Progress("Thermalization", NTHERM)
     productionProgress = cns.meas.Progress("Production", NPROD)
+    logDet = cns.meas.LogDet(kappa, 0, SIGMA_KAPPA)
 
     print("thermalizing")
     phi = cns.hmc.hmc(phi, ham,
-                      cns.hmc.LinearStepLeapfrog(ham, (1, 1), (20, 4), NTHERM-1), NTHERM,
-                      [(1, accMeas), (1, actMeas), (NTHERM/10, thermalizationProgress)],
+                      cns.hmc.LinearStepLeapfrog(ham, (1, 1), (N_LEAPFROG_THERM, N_LEAPFROG), NTHERM-1), NTHERM,
+                      [
+                          (1, accMeas), 
+                          (1, actMeas), 
+                          (NTHERM/10, thermalizationProgress)
+                      ],
                       [(20, cns.checks.realityCheck)])
     print("thermalized!")
 
     # print("running production")
     # detMeas = DetMeas(cns.HubbardFermiMatrix(kappa, 0, SIGMA_KAPPA))
     print("running production")
-    phi = cns.hmc.hmc(phi, ham, cns.hmc.ConstStepLeapfrog(ham, 1, 4), NPROD,
-                      [(1, accMeas), (1, actMeas), (500, productionProgress)])
+    phi = cns.hmc.hmc(phi, ham, cns.hmc.ConstStepLeapfrog(ham, 1, N_LEAPFROG), NPROD,
+                      [
+                          (1, accMeas), 
+                          (1, actMeas), 
+                          (500, productionProgress), 
+                          (1, logDet)
+                      ])
 
     print("processing results")
     ax = accMeas.report(20)
@@ -66,6 +80,9 @@ def main():
 
     ax = actMeas.report(20)
     ax.axvline(NTHERM, c="k")  # mark thermalization - production border
+
+    ax = logDet.report(cns.PH.PARTICLE)
+    ax = logDet.report(cns.PH.HOLE)
 
     plt.show()
 
