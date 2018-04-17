bfcad19ef4b5d23ec6de2afc03c6df7dcb2f5041
Evan Berkowitz <git@evanberkowitz.com>
2018-04-17 21:14:27 +0200

    pylint common.py

--- modules/cns/meas/common.py
+++ modules/cns/meas/common.py
@@ -13,13 +13,20 @@ def newAxes(title, xlabel, ylabel):
     ax.set_ylabel(ylabel)
     return fig, ax
 
-def ensureH5GroupExists(the_file, group):
+def ensureH5GroupExists(theFile, group):
+    r"""!
+    Ensure a path in an HDF5 file exists.
+    The `tables` module lets you `create_group` but only if the parent group already exists.
+    Sometimes you want to create a group deep down in a hierarchy that may only partially exist.
+    \param theFile An open HDF5 file where you wish `/a/group/like/this` to exist.
+    \param group A string of `/slash/delimited/groups/with/no/trailing/slash`.
+    """
     if group[0] != "/":
         group = "/" + group
-    
-    gs = group.split("/")
-    paths = [ "/" + "/".join(gs[1:i]) for i in range(1,len(gs)) ]
 
-    for prev,new in zip(paths,gs[1:]):
-        if prev+"/"+new not in the_file:
-            the_file.create_group(prev,new)
+    gs = group.split("/")
+    paths = ["/" + "/".join(gs[1:i]) for i in range(1, len(gs))]
+
+    for prev, new in zip(paths, gs[1:]):
+        if prev+"/"+new not in theFile:
+            theFile.create_group(prev, new)
