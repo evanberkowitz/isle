0c8da2ed4508f8af1e1178440654b65ffe3d9c06
Evan Berkowitz <git@evanberkowitz.com>
2018-04-17 21:32:50 +0200

    pylint corr_1p

--- modules/cns/meas/corr_1p.py
+++ modules/cns/meas/corr_1p.py
@@ -6,7 +6,6 @@ import numpy as np
 import cns
 
 from .common import newAxes, ensureH5GroupExists
-from ..util import binnedArray
 
 class Corr_1p:
     r"""!
@@ -26,48 +25,66 @@ class Corr_1p:
         nx = len(self.irreps[0])  # this should give the number of ions
         rhss = []
         for eigenstate in self.irreps:
-            for t in range(self.nt):
+            for time in range(self.nt):
                 vec = np.zeros(nx*self.nt, dtype=complex)
-                vec[t*nx:(t+1)*nx] = eigenstate
+                vec[time*nx:(time+1)*nx] = eigenstate
                 rhss.append(cns.Vector(vec))
         # for the t^th spacetime vector of the i^th state, go to self.rhss[i * nt + t]
 
         res = cns.solveM(self.hfm, phi, cns.Species.PARTICLE, rhss)  # this solves M.x = b for the different timeslices and eigenstates
         for i in range(self.numIrreps):
-            corr = [0 for t in range(self.nt)]
+            corr = [0 for time in range(self.nt)]
             for t0 in range(self.nt):
-                for t in range(t0, t0+self.nt):
-                    if(t < self.nt):
-                        corr[t-t0] += np.vdot(rhss[i*self.nt + t], res[i*self.nt + t0])
-                    elif(t >= self.nt):  # this takes into account the anti-periodic boundary conditions
-                        corr[t-t0] -= np.vdot(rhss[i*self.nt + t-self.nt], res[i*self.nt + t0])
-            for t in range(self.nt):  # now average over initial timeslice and append to correlator
-                corr[t] /= self.nt
-                self.corr[i][t].append(corr[t])
-            
-                
+                for time in range(t0, t0+self.nt):
+                    if time < self.nt:
+                        corr[time-t0] += np.vdot(rhss[i*self.nt + time], res[i*self.nt + t0])
+                    elif time >= self.nt:  # this takes into account the anti-periodic boundary conditions
+                        corr[time-t0] -= np.vdot(rhss[i*self.nt + time-self.nt], res[i*self.nt + t0])
+            for time in range(self.nt):  # now average over initial timeslice and append to correlator
+                corr[time] /= self.nt
+                self.corr[i][time].append(corr[time])
+
+
     def report(self, ax=None):
+        r"""!
+        Produce a log-scale plot of the correlation functions.
+        """
+
         if ax is None:
             fig, ax = newAxes("Correlator", r"t", r"C")
             doTightLayout = True
-        
-        c = np.array(self.corr)
-        
-        x = range(self.nt)
-        # x=[ range(self.nt) for i in range(self.numEigenstates) ]
-        avg = np.mean(c, axis=2)
-        err = np.std(c, axis=2)
-        
+
+        correlator = np.array(self.corr)
+
+        timeSlice = range(self.nt)
+        avg = np.mean(correlator, axis=2)
+        err = np.std(correlator, axis=2)
+
         ax.set_yscale("log")
-        
-        for m,s in zip(avg,err):
-            ax.errorbar(x, m, yerr=s)
-            
-    def save(self, the_file, path):
-        ensureH5GroupExists(the_file, path)
-        the_file.create_array(path, "correlators", np.array(self.corr))
-        the_file.create_array(path, "irreps", self.irreps)
-    
-    def read(self, the_file, path):
-        self.corr = the_file.get_node(path+"/correlators").read()
-        self.irreps = the_file.get_node(path, "irreps").read()
+
+        for mean, stdDev in zip(avg, err):
+            ax.errorbar(timeSlice, mean, yerr=stdDev)
+
+        if doTightLayout:
+            fig.tight_layout()
+
+        return ax
+
+    def save(self, theFile, path):
+        r"""!
+        Write the irreps and their correlators to a file.
+        \param theFile An open HDF5 file.
+        \param path Where to write to in theFile
+        """
+        ensureH5GroupExists(theFile, path)
+        theFile.create_array(path, "correlators", np.array(self.corr))
+        theFile.create_array(path, "irreps", self.irreps)
+
+    def read(self, theFile, path):
+        r"""!
+        Read the irreps and their correlators from a file.
+        \param theFile An open HDF5 file.
+        \param path Where to read from in theFile.
+        """
+        self.corr = theFile.get_node(path+"/correlators").read()
+        self.irreps = theFile.get_node(path, "irreps").read()
