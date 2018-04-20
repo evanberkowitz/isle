d7253daee5abfcdd506190e35a0cbf68f1ca4c3a
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-04-20 11:15:12 +0200

    Add ensemble.readH5

--- modules/cns/ensemble.py
+++ modules/cns/ensemble.py
@@ -3,6 +3,8 @@ Handle ensembles.
 """
 
 import importlib.util
+import tempfile
+from pathlib import Path
 
 import yaml
 
@@ -31,6 +33,27 @@ def writeH5(ensembleFile, group, name="ensemble"):
     """
     with open(str(ensembleFile), "r") as ensf:
         group[name] = ensf.read()
+
+def readH5(group, name="ensemble"):
+    r"""!
+    Read ensemble from HDF5.
+
+    Needs the ensemble module to be represented as a string in the HDF5.
+    \param group HDF5 group that contains the ensemble string.
+    \param name Name of the ensemble dataset in group.
+    """
+
+    with tempfile.NamedTemporaryFile(mode="w", suffix=".py") as modf:
+        # just write it to a temporary file
+        modf.write(group[name][()])
+        modf.flush()
+
+        # and read it back in
+        moduleName = str(Path(group.filename).stem)
+        spec = importlib.util.spec_from_file_location(moduleName, modf.name)
+        ensemble = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(ensemble)
+    return ensemble
 
 def readLattice(filename):
     r"""!
