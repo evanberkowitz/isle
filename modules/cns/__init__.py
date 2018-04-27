f98abb5d2abac0c76a9401c804291aa920c31c8d
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-04-27 14:03:51 +0200

    yaml_io -> yamlio

--- modules/cns/__init__.py
+++ modules/cns/__init__.py
@@ -5,13 +5,14 @@ Contains Python modules and imports everything from cnxx into the cns namespace.
 """
 
 from cns.cnxx_wrappers import *
-import cns.yaml_io
-import cns.hmc
 import cns.checks
+import cns.ensemble
+import cns.yamlio
+import cns.h5io
+import cns.hmc
 import cns.random
 import cns.util
-import cns.h5io
-import cns.ensemble
+from .hubbardFermiActionSpinBasis import HubbardFermiActionSpinBasis
 
 ## Environment variables for CNS.
 env = {}
