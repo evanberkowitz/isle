e61089f40e352486ef4a3ab91f877e0919d14929
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-04-27 14:07:51 +0200

    Bundle IO modules in fileio

--- modules/cns/__init__.py
+++ modules/cns/__init__.py
@@ -7,8 +7,7 @@ Contains Python modules and imports everything from cnxx into the cns namespace.
 from cns.cnxx_wrappers import *
 import cns.checks
 import cns.ensemble
-import cns.yamlio
-import cns.h5io
+import cns.fileio
 import cns.hmc
 import cns.random
 import cns.util
---
+++ modules/cns/fileio.py
@@ -0,0 +1,6 @@
+"""!
+General file input / output.
+"""
+
+from . import yamlio as yaml
+from . import h5io as h5
