cf9ff23003b2bb6d744af6da5a19e71c539a540d
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-04-27 14:55:59 +0200

    Add loadEnsemble()

--- modules/cns/ensemble.py
+++ modules/cns/ensemble.py
@@ -7,6 +7,7 @@ import tempfile
 from pathlib import Path
 
 import yaml
+import h5py as h5
 
 import cns
 
@@ -15,6 +16,8 @@ def importEnsemble(ensembleFile):
     Import an ensemble Python module from a given file location.
     \param ensembleFile Path to the ensemble module to load.
     """
+
+    ensembleFile = str(ensembleFile) # so we can deal with pathlib.Path
 
     # As explained in
     # https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
@@ -53,6 +56,29 @@ def readH5(group, name="ensemble"):
         spec = importlib.util.spec_from_file_location(moduleName, modf.name)
         ensemble = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(ensemble)
+    return ensemble
+
+def loadEnsemble(infname, inftype):
+    r"""!
+    Load an ensemble from a file.
+    \param infname Name of the input file.
+    \param inftype Type of the input file, allowed values are `cns.fileio.FileType.PY`
+                   and `cns.fileio.FileType.HDF5`.
+    \returns The ensemble.
+    """
+
+    if inftype == cns.fileio.FileType.PY:
+        # load from Python module
+        ensemble = cns.ensemble.importEnsemble(infname)
+    elif inftype == cns.fileio.FileType.HDF5:
+        # load from HDF5 file
+        with h5.File(infname, "r") as h5f:
+            ensemble = cns.ensemble.readH5(h5f)
+    else:
+        # no other file types supported
+        raise ValueError("Cannot load ensemble from file type '{}', file = {}" \
+                         .format(str(inftype).split(".")[1].lower(), infname))
+
     return ensemble
 
 def readLattice(filename):
