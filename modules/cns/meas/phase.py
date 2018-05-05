c7c86d73c02bc1e4aa3fe6654977ecba7073f71c
Evan Berkowitz <git@evanberkowitz.com>
2018-05-05 16:46:58 +0200

    Monte Carlo History Script
    Add a script to do some summaries of HMC.
    
    - Changed bin size default to 1, so it always divides the number of samples.
    - Added 2D histogram of the weight, made it the default "accompanying" plot rather than a histogram of the phase itself (where the KDE would need to know about the 2π periodicity for an honest extraction of the kernel).

--- modules/cns/meas/phase.py
+++ modules/cns/meas/phase.py
@@ -3,8 +3,11 @@ Measurement of action.
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
 
-from .common import newAxes
+
+from .common import newAxes, oneDimKDE
 from ..util import binnedArray
 from ..h5io import createH5Group
 
@@ -21,15 +24,45 @@ class Phase:
         """!Record phase."""
         self.theta.append(np.imag(kwargs["act"]))
 
-    def report(self, binsize=1, ax=None, fmtre="", fmtim=""):
+    def report(self, axes=None, **kwargs):
+        if axes is None:
+            spacer = 0.05
+            left, width = 0.1, 0.5
+            bottom, height = 0.1, 0.8
+
+            fig = plt.figure()
+            histax = plt.axes([left, bottom, width, height])
+            distax = plt.axes([left+width+spacer, bottom, 1-left-width-2*spacer, height])
+            doTightLayout = False
+        
+        else:  # use Axes that were passed in
+            fig = plt.gcf()
+            histax, distax = axes
+            doTightLayout = True
+        
+        # history
+        self.plotMCHistory(histax, alpha=0.75)
+        histax.set_title(r"Monte Carlo History of $\theta$")
+        histax.set_xlabel(r"$N_{\mathrm{tr}}$")
+        histax.set_ylabel(r"$\theta$")
+        histax.tick_params(right=True)
+
+        # 2D histogram
+        self.plotHist2D(distax)
+        distax.set_title(r"Histogram of $e^{-i\theta}$")
+
+        if doTightLayout:
+            fig.tight_layout()
+        return (histax, distax)
+        
+
+    def plotMCHistory(self, ax=None, binsize=1, color="orange", **kwargs):
         r"""!
-        Plot the phase against Monte Carlo time.
+        Plot monte carlo history of the phase.
         \param binsize The phase is averaged over `binsize` trajectories.
         \param ax Matplotlib Axes to plot in. If `None`, a new one is created in a new figure.
-        \param fmtre Plot format passed to matplotlib.
-                     Can encode color, marker and line styles of real part.
-        \param fmtim Plot format passed to matplotlib.
-                     Can encode color, marker and line styles of imaginary part.
+        \param color Line color of the plot.
+        \param kwargs Passed to `matplotlib.Axes.plot`.
         \returns The Axes with the plot.
         """
 
@@ -42,10 +75,88 @@ class Phase:
             doTightLayout = True
 
         mcTime = np.arange(0, len(self.theta), binsize)
-        ax.plot(mcTime, np.real(binned), fmtre, label=r"$\theta$")
-        ax.legend()
+        if "label" in kwargs:
+            ax.plot(np.arange(len(self.theta)), np.real(self.theta), color=color, **kwargs)
+        else:
+            ax.plot(np.arange(len(self.theta)), np.real(self.theta),
+                    label=r"$\theta", color=color, **kwargs)
+
         if doTightLayout:
             fig.tight_layout()
+        return ax
+    
+    def plotDistribution(self, ax=None, hist=True, kde=True, orientation="vertical", facecolor="orange", color="black", **kwargs):
+        
+        r"""!
+        Plot histogram of complex part of the action.
+        \param ax Matplotlib Axes to plot in. If `None`, a new one is created in a new figure.
+        \param hist Choose whether to show histogram.
+        \param kde Choose whether to show kernel densitiy estimation.
+        \param orientation Set to `"horizontal"` to rotate plot 90°.
+        \param facecolor Color of the histogram.
+        \param color Line color of kernel density estimate.
+        \param kwargs Passed to `matplotlib.Axes.hist` <B>and</B> `matplotlib.Axes.plot`.
+        \returns The Axes with the plot.
+        """
+        
+        # make a new axes is needed
+        doTightLayout = False
+        if ax is None:
+            fig, ax = newAxes("", r"$\Phi$", r"PDF")
+            doTightLayout = True
+        
+        theta = self.theta
+        
+        if hist:    # MC history
+            ax.hist(theta, 50, normed=1, facecolor=facecolor, orientation=orientation, label="histogram", **kwargs)
+        
+        if kde:     # kernel density estimation
+            samplePts, dens = oneDimKDE(theta, bandwidth=3/5)
+            if orientation == "vertical":
+                ax.plot(samplePts, dens, color=color, label="kde", **kwargs)
+            elif orientation == "horizontal":
+                ax.plot(dens, samplePts, color=color, label="kde", **kwargs)
+            else:
+                raise RuntimeError(f"Uknown orientation: {orientation}")
+            ax.set_xlim((0, np.max(dens)*1.1))
+        
+        if doTightLayout:
+            fig.tight_layout()
+        
+        return ax
+
+    def plotHist2D(self, ax=None, **kwargs):
+        
+        r"""!
+        Plot histogram of complex part of the action.
+        \param ax Matplotlib Axes to plot in. If `None`, a new one is created in a new figure.
+        \param kwargs Passed to `matplotlib.Axes.hist2d` <B>and</B> `matplotlib.Axes.plot`.
+        \returns The Axes with the plot.
+        """
+        
+        # make a new axes is needed
+        doTightLayout = False
+        if ax is None:
+            fig, ax = newAxes("Weight Histogram", "", "", figsize=(10,10))
+            doTightLayout = True
+        
+        theta = np.array(self.theta)
+        weight = np.exp(-theta*1j)
+        x = np.real(weight)
+        y = np.imag(weight)
+        
+        view = [-1.05, 1.05]
+        h = ax.hist2d(x, y, bins=51, range=[view, view], normed=1, label="histogram", **kwargs)
+        ax.set_xlim(view)
+        ax.set_ylim(view)
+        ax.set_aspect(1)
+        plt.colorbar(h[3])
+        
+        # Indicate the average weight
+        x_mean = np.mean(x)
+        y_mean = np.mean(y)
+        ax.scatter([x_mean], [y_mean], c="w", )
+        
         return ax
 
     def save(self, base, name):
---
+++ scripts/monte-carlo-history.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""!
+Produce a report of measurement output.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import h5py as h5
+import os, sys
+from pathlib import Path
+import argparse
+
+import core
+core.prepare_module_import()
+import cns
+import cns.meas
+
+def symmetrize(a):
+    return 0.5*(a + a.conj().T)
+
+def main(args):
+    """!Analyze HMC results."""
+    
+    cns.env["latticeDirectory"] = Path(__file__).resolve().parent.parent/"lattices"
+    
+    with h5.File(args.ensemble,'r') as measFile:
+        ensemble, ensembleText = cns.ensemble.loadH5("ensemble", measFile)
+    
+    
+    monteCarloDiagnostics = [
+        # cns.meas.Action(),
+        # cns.meas.TotalPhi(),
+        cns.meas.Phase()
+    ]
+    
+    with h5.File(args.ensemble,'r') as measurementFile:
+        for cfg in measurementFile["configuration"]:
+            action = measurementFile["configuration"][cfg]["action"][()]
+            field  = measurementFile["configuration"][cfg]["phi"][()]
+            for diagnostic in monteCarloDiagnostics:
+                diagnostic(field, act=action)
+    
+    
+    for diagnostic in monteCarloDiagnostics:
+        ax = diagnostic.report()
+    
+    plt.show()
+    
+    return 0
+
+def parseArgs():
+    "Parse command line arguments."
+    parser = argparse.ArgumentParser(description="""
+    Produce a measurement report.
+    """)
+    parser.add_argument("ensemble", help="Ensemble module")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    main(parseArgs())
