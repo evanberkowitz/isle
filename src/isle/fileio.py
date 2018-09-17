7a7fba2dac2a1482442e7f6b51e919a91379e6cb
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-09-17 15:15:48 +0200

    Add functions to handle source of functions

--- src/isle/fileio.py
+++ src/isle/fileio.py
@@ -4,6 +4,7 @@ General file input / output.
 
 from enum import Enum, auto
 from pathlib import Path
+import inspect
 
 # import those to make them available outside of the package
 from . import yamlio as yaml  # (unused import) pylint: disable=W0611
@@ -36,3 +37,69 @@ def fileType(path):
 def pathAndType(fname):
     """!Return path and type of file."""
     return Path(fname), fileType(fname)
+
+def sourceOfFunction(func):
+    """!
+    Return the source code of a function.
+    Works only on free functions, not methods or lambdas.
+
+    \see functionFromSource for the inverse.
+    """
+
+    if not inspect.isfunction(fn) or inspect.ismethod(fn):
+        raise RuntimeError("Not a function, methods not allowed")
+    # missing a check for class methods!
+
+    src = inspect.getsource(fn)
+
+    # lambdas are weird, need do make sure this is a normal function definition
+    if not src.lstrip().startswith("def"):
+        raise RuntimeError("Not a proper function, did you pass a lambda?")
+
+    return src
+
+def functionFromSource(src):
+    """!
+    Return a function defined by a piece of source code.
+
+    \see sourceOfFunction for the inverse.
+    """
+
+    # strip indentation of function definition
+    src = src.lstrip()
+
+    # an empty local scope
+    scope = dict()
+    # no access to globals so it can't fuck around
+    exec(src, {}, scope)
+
+    if not scope:
+        raise RuntimeError("Nothing defined by source code")
+    if len(scope) > 1:
+        raise RuntimeError("More than one thing defined")
+
+    # the only thing in scope is the one we want
+    obj = list(scope.values())[0]
+    if not inspect.isfunction(obj):
+        raise RuntimeError("Source code defined something else than a function")
+
+    return obj
+
+def callFunctionFromSource(src, *args, **kwargs):
+    """!
+    Extract a function from source code and call it.
+
+    `args` and `kwargs` are passed on to the function and its result is returned.
+
+    \see soruceOfFunction and functionFromSource.
+    """
+
+    func = functionFromSource(src)
+    try:
+        return func(*args, **kwargs)
+    except NameError as e:
+        print(f"Undefined symbol in function constructed from source: {e}")
+        raise
+    except:
+        # must re-raise it so things like keyboard interrupts get processed properly
+        raise
