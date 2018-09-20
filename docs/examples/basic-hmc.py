ba8ec425435a719cc132ea339797edad9baf00d6
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-09-20 13:22:11 +0200

    Add (mostly undocumented) hmc script example

---
+++ docs/examples/basic-hmc.py
@@ -0,0 +1,61 @@
+"""!
+Example script to produce configurations with HMC.
+"""
+
+import yaml
+
+import isle
+import isle.drivers
+
+# initialize command line interface and parse command line arguments
+cla = isle.cli.init('hmc', name='basic-hmc')
+
+# load lattice
+with open('resources/lattices/four_sites.yml', 'r') as latfile:
+    lat = yaml.safe_load(latfile.read())
+lat.nt(16)  # lattice files usually only contain information on spatial lattice
+
+# specify model parameters
+params = isle.util.parameters(beta=3, U=2, mu=0, sigmaKappa=-1,
+                              alpha=1, hopping=isle.action.Hopping.DIAG)
+
+# set up a random number generator
+rng = isle.random.NumpyRNG(1075)
+
+# define an action
+def makeAction(lat, params):
+    import isle
+    import isle.action
+
+    delta = params.beta / lat.nt()
+    UTilde = params.U * delta
+    muTilde = params.mu * delta
+
+    return isle.Hamiltonian(isle.action.HubbardGaugeAction(UTilde),
+                            isle.action.makeHubbardFermiAction(lat,
+                                                               params.beta,
+                                                               muTilde,
+                                                               params.sigmaKappa,
+                                                               params.alpha,
+                                                               params.hopping))
+
+# set up a fresh HMC driver to control HMC evolution
+hmcState = isle.drivers.hmc.init(lat, params, rng, makeAction, cla.outfile,
+                                 cla.overwrite, startIdx=0)
+
+# random initial condition
+phi = isle.Vector(rng.normal(0, (params.U * params.beta / lat.nt())**(1/2), lat.lattSize())+0j)
+
+print("termalizing")
+# a proposer to linearly decrease the number of MD steps
+proposer = isle.proposers.LinearStepLeapfrog(makeAction(lat, params), (1, 1), (20, 5), 99)
+# thermalize configuration without saving anything
+phi = hmcState(phi, proposer, 100, saveFreq=0, checkpointFreq=0)
+# reset the internal counter so we start saving configs at index 0
+hmcState.resetIndex()
+
+print("producing")
+# new proposer with a constant number of steps
+proposer = isle.proposers.ConstStepLeapfrog(hmcState.ham, 1, 5)
+# produce configurations and save in intervals
+phi = hmcState(phi, proposer, 10000, saveFreq=2, checkpointFreq=10)
