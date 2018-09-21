214dea8996e8ffb8ed0ac96fc04b4434d22e4da5
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-09-21 10:02:56 +0200

    Clean up Action measurement

--- src/isle/meas/action.py
+++ src/isle/meas/action.py
@@ -2,78 +2,43 @@
 Measurement of action.
 """
 
-import numpy as np
-
-from .common import newAxes
-from ..util import binnedArray
 from ..h5io import createH5Group
 
 class Action:
     r"""!
     \ingroup meas
-    Measure the action.
+    Transfer the action from a configuration file into a measurement file.
+    Does nothing if the measurement file already contains the action.
     """
 
     def __init__(self):
-        self.act = []
+        self.action = []
 
-    def __call__(self, phi, inline=False, **kwargs):
+    def __call__(self, phi, action, itr):
         """!Record action."""
-        self.act.append(kwargs["act"])
-
-    def report(self, binsize=1, ax=None, fmtre="", fmtim=""):
-        r"""!
-        Plot the action against Monte Carlo time.
-        \param binsize The action is averaged over `binsize` trajectories.
-        \param ax Matplotlib Axes to plot in. If `None`, a new one is created in a new figure.
-        \param fmtre Plot format passed to matplotlib.
-                     Can encode color, marker and line styles of real part.
-        \param fmtim Plot format passed to matplotlib.
-                     Can encode color, marker and line styles of imaginary part.
-        \returns The Axes with the plot.
-        """
-
-        doTightLayout = False
-        binned = binnedArray(self.act, binsize)
-        if ax is None:
-            fig, ax = newAxes(r"Action: global mean of <$S$> = {:3.5f}+{:3.5f}*I+/-{:3.5f}+{:3.5f}*I".format(np.mean(np.real(binned)),
-                                                                                                             np.mean(np.imag(binned)),
-                                                                                                             np.std(np.real(binned)),
-                                                                                                             np.std(np.imag(binned))),
-                              r"$N_{\mathrm{tr}}$", r"$S$")
-            doTightLayout = True
-
-        mcTime = np.arange(0, len(self.act), binsize)
-        ax.plot(mcTime, np.real(binned), fmtre, label="Re($S$)")
-        ax.plot(mcTime, np.imag(binned), fmtim, label="Im($S$)")
-        ax.legend()
-        if doTightLayout:
-            fig.tight_layout()
-        return ax
+        self.action.append(action)
 
     def save(self, base, name):
         r"""!
         Write the action to a file.
         \param base HDF5 group in which to store data.
-        \param name Name of the subgroup ob base for this measurement.
+        \param name Name of the subgroup of base for this measurement.
         """
         group = createH5Group(base, name)
-        group["action"] = self.act
+        if not "action" in group and not "configuration" in group \
+           and not "configuration" in base:
+            group["action"] = self.action
 
-    def read(self, group, fromCfgFile=False):
+    def read(self, group):
         r"""!
         Read the action from a file.
         \param group HDF5 group which contains the data of this measurement.
-        \param fromCfgFile
-               - `True`: Read data from a configuration file as written by
-                          isle.meas.WriteConfiguration.
-               - `False`: Read data from a single dataset as written by this measurement.
         """
-        try:
-            if fromCfgFile:
-                self.act = [group[cfg]["action"][()] for cfg in group]
-            else:
-                self.act = group["action"][()]
-        except KeyError:
-            raise RuntimeError("No dataset 'action' found in group {} in file {}"\
-                               .format(group.name, group.file)) from None
+        if "action" in group:
+            self.action = group["action"][()]
+        elif "configuration" in group:
+            cfgGrp = group["configuration"]
+            self.action = [cfgGrp[cfg]["action"][()] for cfg in cfgGrp]
+        else:
+            print(f"Error: no action found in HDF5 group '{group}'."
+                  " Did not find subgroups 'action' or 'configuration'.")
