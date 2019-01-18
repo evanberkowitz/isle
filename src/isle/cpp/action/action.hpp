bee47765dd2637e6290f3cc30799eb15e645141f
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2019-01-18 15:30:15 +0100

    Make Action's d'tor noexcept

--- src/isle/cpp/action/action.hpp
+++ src/isle/cpp/action/action.hpp
@@ -12,7 +12,7 @@ namespace isle {
     namespace action {
         /// Abstract base for Actions.
         struct Action {
-            virtual ~Action() = default;
+            virtual ~Action() noexcept = default;
 
             /// Evaluate the %Action for given auxilliary field phi.
             virtual std::complex<double> eval(const Vector<std::complex<double>> &phi) const = 0;
