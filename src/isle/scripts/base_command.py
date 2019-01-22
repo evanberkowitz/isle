21768c6a576d71a5c0c0ffe11b51327f3d1b88df
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2019-01-22 10:50:58 +0100

    Define parsers in base_command instead of cli

--- src/isle/scripts/base_command.py
+++ src/isle/scripts/base_command.py
@@ -5,8 +5,11 @@ Dispatches calls to other, specific scripts.
 
 from logging import getLogger
 
-from .. import initialize
+from .. import initialize, cli
 from . import continuation
+
+## Show at the end of help messages.
+EPILOG = "See https://github.com/jl-wynen/isle"
 
 
 def _run_show(args):
@@ -14,27 +17,45 @@ def _run_show(args):
     from .show import main
     main(args)
 
+def _makeParser():
+    # map command names to properties
+    commands = {"show": ("Pulls all data it can from a file in a format supported by Isle, "
+                         "prints, and visualizes that data. Supported file types are HDF5 and YAML. "
+                         "Select a reporter via -r to choose which information to show.",
+                         cli.addShowArgs,
+                         "none"),
+
+                "continue": ("Perform a continuation HMC run. Loads a checkpoint and produce more "
+                             "configurations with the same parameters and proposer.",
+                             cli.addContinueArgs,
+                             "isle.hmc.log")}
+
+    # construct a base parser
+    parser = cli.makeDefaultParser("isle",
+                                   description="Base utility program of Isle. Dispatches to sub-commands. "
+                                   "Use -h for a sub-command to get more information.",
+                                   epilog=EPILOG)
+
+    # add all sub parsers
+    subp = parser.add_subparsers(title="Commands", dest="cmd", required=True)
+    for command, (description, argFn, defaultLog) in commands.items():
+        subParser = subp.add_parser(command, epilog=EPILOG,
+                                    description=description)
+        argFn(cli.addDefaultArgs(subParser, defaultLog=defaultLog))
+
+    return parser
+
 def main():
     """!Run Isle's base script. Dispatches to other scripts based on command line arguments."""
 
-    commands = ("show", "continue")
-    mains = (_run_show, continuation.main)
-    descriptions = ("Pulls all data it can from a file in a format supported by Isle, "
-                    "prints, and visualizes that data. Supported file types are HDF5 and YAML. "
-                    "Select a reporter via -r to choose which information to show.",
+    mains = {"show": _run_show,
+             "continue": continuation.main}
 
-                    "Perform a continuation HMC run. Loads a checkpoint and produce more "
-                    "configurations with the same parameters and proposer.")
-
-    args = initialize(commands,
-                      name="isle",
-                      description="Base utility program of Isle. Dispatches to sub-commands. "
-                      "Use -h for a sub-command to get more information.",
-                      epilog="See https://github.com/jl-wynen/isle",
-                      subdescriptions=descriptions)
+    parser = _makeParser()
+    args = initialize(parser)
 
     try:
-        mains[commands.index(args.cmd)](args)
+        mains[args.cmd](args)
     except:
         getLogger("isle").exception("Failed to execute commad %s", args.cmd)
         exit(1)
