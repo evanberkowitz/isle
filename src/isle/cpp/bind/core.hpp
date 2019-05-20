ba4cc68059aa03a1d02b997396b3a8ee7532d8be
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2019-05-20 11:57:53 +0200

    No longer bind std::vector to Python

--- src/isle/cpp/bind/core.hpp
+++ src/isle/cpp/bind/core.hpp
@@ -21,9 +21,6 @@
 
 namespace py = pybind11;
 
-// use a custom type
-PYBIND11_MAKE_OPAQUE(std::vector<isle::CDVector>)
-
 /// Bindings between `isle` and Python.
 namespace bind {
     /// Represents the state of an iterator (adapted from pybind11 internals).
