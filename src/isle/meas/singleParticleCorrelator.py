9556630734ced33ffd274b913b0e1c28239ea637
Evan Berkowitz <git@evanberkowitz.com>
2019-06-17 13:50:35 +0200

    Add singleParticleCorrelator measurement.

--- src/isle/meas/singleParticleCorrelator.py
+++ src/isle/meas/singleParticleCorrelator.py
@@ -3,15 +3,13 @@ r"""!\file
 Measurement of single-particle correlator.
 """
 
-from logging import getLogger
-
 import numpy as np
-# import scipy.linalg as la   # scipy is needed because np.linalg.eig and np.linalg.eigh do not guarantee an orthonormal eigenbasis.
 
 import isle
 from .measurement import Measurement
-from ..util import spaceToSpacetime, rollTemporally
+from ..util import spaceToSpacetime, temporalRoller
 from ..h5io import createH5Group
+from .propagator import AllToAll
 
 class SingleParticleCorrelator(Measurement):
     r"""!
@@ -19,61 +17,46 @@ class SingleParticleCorrelator(Measurement):
     Tabulate single-particle correlator.
     """
 
-    def __init__(self, hfm, species, savePath, configSlice=slice(None, None, None), alpha=1, projector=None):
+    def __init__(self, allToAll, savePath, configSlice=slice(None, None, None), projector=None):
         super().__init__(savePath, configSlice)
 
-        self.hfm = hfm
-        self.corr = []
-        self.species = species
-        self._alpha = alpha
+        self.correlators = []
+        self._inverter = allToAll
+        self._path = None
+        self._indices = "idf,bx,xfyi,ya->bad"
+        self._roll = None
 
-        # self.irreps = np.transpose(la.orth(isle.Matrix(hfm.kappaTilde())))
+        self.nx = self._inverter.nx
+        self.nt = None
+
         if projector is None:
-            _, self.irreps = np.linalg.eigh(isle.Matrix(hfm.kappaTilde()))
+            _, self.irreps = np.linalg.eigh(isle.Matrix(allToAll.hfm.kappaTilde()))
             self.irreps = self.irreps.T
         else:
             self.irreps = projector.T
-        # self.irreps = np.identity(self.hfm.nx())
 
-        # need to know Nt to set those, do it in _getRHSs
-        self._rhss = None
-
-        if species == isle.Species.PARTICLE:
-            getLogger(__name__).info("Initialized for particles, irreps:\n %s", self.irreps)
-        elif species == isle.Species.HOLE:
-            getLogger(__name__).info("Initialized for holes, irreps:\n %s", self.irreps)
-        else:
-            getLogger(__name__).error("Unknown species: %s", species)
-            raise ValueError(f"Unknown species: {species}")
+        self.irreps = np.matrix(self.irreps)
 
     def __call__(self, phi, action, itr):
         """!Record the single-particle correlators."""
 
-        nt = int(len(phi) / self.hfm.nx())
-        rhss = self._getRHSs(nt)
+        S = self._inverter(phi, action, itr)
 
-        # Solve M*x = b for all right-hand sides:
-        if self._alpha == 1:
-            res = np.array(isle.solveM(self.hfm, phi, self.species, rhss), copy=False)
-        else:
-            res = np.linalg.solve(isle.Matrix(self.hfm.M(-1j*phi, self.species)), np.array(rhss).T).T
+        if self.nt is None:
+            self.nt = len(phi) // self.nx
+        if self._roll is None:
+            self._roll = np.array([temporalRoller(self.nt, -t) for t in range(self.nt)])
 
-        propagators = res.reshape([len(self.irreps), nt, nt, len(self.irreps)])
-        # The logic for the one-liner goes as:
-        # For each source irrep we need to apply a sink for every irrep.
-        #     This produces a big cross-correlator.
-        # For each source time we need to roll the vector such that the
-        #     source lives on timeslice 0.
-        # Finally, we need to average over all the source correlators with
-        #     the same source irrep but different timeslices.
-        self.corr.append(np.mean(
-            np.array([
-                [
-                    [rollTemporally(np.dot(propagators[i, src], np.conj(irrepj)), -src)
-                     for src in range(nt)]
-                    for i in range(len(self.irreps))]
-                for irrepj in self.irreps]),
-            axis=2))
+        self._tensors = (self._roll, self.irreps, S, self.irreps.H)
+
+        if self._path is None:
+            self._path, _ = np.einsum_path(self._indices, *self._tensors, optimize='optimal')
+
+        # The temporal roller sums over time, but does not *average* over time.  So, divide by nt:
+        correlator = np.einsum(self._indices, *self._tensors, optimize=self._path) / self.nt
+
+        # Done!
+        self.correlators.append(correlator)
 
     def save(self, h5group):
         r"""!
@@ -81,22 +64,9 @@ class SingleParticleCorrelator(Measurement):
         \param h5group Base HDF5 group. Data is stored in subgroup `h5group/self.savePath`.
         """
         subGroup = createH5Group(h5group, self.savePath)
-        subGroup["correlators"] = self.corr
+        subGroup["correlators"] = self.correlators
         subGroup["irreps"] = self.irreps
-
-    def _getRHSs(self, nt):
-        """!
-        Get all right hand side vectors as a matrix.
-        For the j^th spacetime vector of the i^th state, go to self.rhss[i * nt + j]
-        In other words, time is the faster running index.
-        """
-
-        if self._rhss is None or self._rhss.rows() != nt*self.hfm.nx():
-            # Create a large set of sources:
-            self._rhss = isle.Matrix(np.array([isle.Vector(spaceToSpacetime(irrep, time, nt))
-                                               for irrep in self.irreps for time in range(nt)]))
-        return self._rhss
-
+        subGroup["einsum_path"] = self._path
 
 def read(h5group):
     r"""!
