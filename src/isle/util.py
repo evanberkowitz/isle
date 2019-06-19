978c37a57dc0596ccc00137bbc855e2fd51f2493
Evan Berkowitz <git@evanberkowitz.com>
2019-06-19 16:38:25 +0200

    ones is different frome eye.

--- src/isle/util.py
+++ src/isle/util.py
@@ -263,7 +263,7 @@ def temporalRoller(nt, dt, fermionic=True):
 def signAlternator(nx, sigmaKappa=-1):
     # The quantity (-sigmaKappa)^x shows up very frequently.
     if sigmaKappa == -1:
-        return np.ones(nx)
+        return np.eye(nx)
     elif sigmaKappa == +1:
         # NOTE: correctness depends on the even-odd indexing corresponding to the bipartition.
         return np.diag([ +1 if np.mod(x,2) == 0 else -1 for x in range(nx) ])
