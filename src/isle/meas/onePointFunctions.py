f1a4d044cd6b6596d7a1832f239dadf6297d7d4e
Evan Berkowitz <git@evanberkowitz.com>
2020-03-18 17:47:56 -0400

    Major improvements to onePointFunctions.

--- src/isle/meas/onePointFunctions.py
+++ src/isle/meas/onePointFunctions.py
@@ -39,6 +39,7 @@ We can transform into a different basis; by default the results are in the spati
 from logging import getLogger
 
 import numpy as np
+import h5py as h5
 
 import isle
 from .measurement import Measurement
@@ -46,6 +47,8 @@ from ..util import temporalRoller
 from ..h5io import createH5Group
 
 fields = ["np", "nh"]
+
+#TODO: save / retrieve einsum paths.
 
 class measurement(Measurement):
     r"""!
@@ -79,8 +82,6 @@ class measurement(Measurement):
         nt = P.shape[1]
 
         d = np.eye(nx*nt).reshape(*P.shape) # A Kronecker delta
-        if self.transform is None:
-            self.transform = np.eye(nx)
 
         log = getLogger(__name__)
 
@@ -88,15 +89,24 @@ class measurement(Measurement):
         data["np"] = d-P
         data["nh"] = d-H
 
-        # We'll time average and transform at once:
         if self._einsum_path is None:
-            self._einsum_path, _ = np.einsum_path("ax,xtxt->a", self.transform, data["np"], optimize="optimal")
-            log.info("Optimized Einsum path for time averaging and unitary transformation.")
+            if self.transform is None:
+                # No need for the transformation, cut the cost:
+                self._einsum_path, _ = np.einsum_path("xtxt->x", data['np'], optimize="optimal")
+                log.info("Optimized Einsum path for time averaging.")
+            else:
+                # We'll time average and transform at once:
+                self._einsum_path, _ = np.einsum_path("ax,xtxt->a", self.transform, data["np"], optimize="optimal")
+                log.info("Optimized Einsum path for time averaging and unitary transformation.")
 
-
-        for correlator in self.data:
-            measurement = np.einsum("ax,xtxt->a", self.transform, data[correlator], optimize=self._einsum_path) / nt
-            self.data[correlator].append(measurement)
+        if self.transform is None:
+            for correlator in self.data:
+                measurement = np.einsum("xtxt->x", data[correlator], optimize=self._einsum_path) / nt
+                self.data[correlator].append(measurement)
+        else:
+            for correlator in self.data:
+                measurement = np.einsum("ax,xtxt->a", self.transform, data[correlator], optimize=self._einsum_path) / nt
+                self.data[correlator].append(measurement)
 
 
     def save(self, h5group):
@@ -104,22 +114,70 @@ class measurement(Measurement):
         \param h5group Base HDF5 group. Data is stored in subgroup `h5group/self.savePath`.
         """
         subGroup = createH5Group(h5group, self.savePath)
-        subGroup["transform"] = self.transform
+        if self.transform is None:
+            subGroup["transform"] = h5.Empty(dtype="complex")
+        else:
+            subGroup["transform"] = self.transform
         for field in self.data:
             subGroup[field] = self.data[field]
 
 def read(h5group):
     r"""!
     \param h5group HDF5 group which contains the data of this measurement.
+
+    \returns A dictionary of measurements and a basis transformation, \n
+    {    \n
+        `np`: \f$n^p_x\f$,                      \n
+        `nh`: \f$n^h_x\f$,                      \n
+        `onePoint-transform`: A transformation from position space to another space \n
+        }
+    where both \f$n^{p,h}_x\f$ have the shape [measurements, spatial dimension] as long as `onePoint-transform` is square.
     """
-    return {key: h5group[key][()] for key in h5group}
+
+    try:
+        data = {key: h5group[key][()] for key in h5group}
+
+        if type(data['transform']) is h5.Empty:
+            data['transform'] = None
+
+        data['onePoint-transform'] = data['transform']
+        del data['transform']
+
+        return data
+    except:
+        raise KeyError(f"Problem reading onePointFunction measurements from {h5group.name}")
 
 def complete(measurements):
+    r"""!
+    \param measurements a dictionary of measurements, like what is returned from read().
 
-    rest = dict()
+    Measurements of one-point functions \f$\rho_x\f$, \f$n_x\f$,
+    and \f$S^3_x\f$ are built from measurements of \f$n^p_x\f$ and \f$n^h_x\f$
+    using the identities above.
 
-    rest["rho"] = measurements["np"] - measurements["nh"]
-    rest["N"]   = measurements["np"] + measurements["nh"]
-    rest["S3"]  = 0.5 * ( 1 - rest["N"])
+    This can be used with the following example codeblock
 
-    return {**measurements, **rest}
+    ```python
+        with h5.File('measurements.h5','r') as f:
+            data = isle.meas.onePointFunctions.read(f["correlation_functions/one_point"])
+
+        data = isle.meas.onePointFunctions.complete(data)
+    ```
+
+    \returns a dictionary with additional one-point functions, built from those already computed.
+    """
+
+    try:
+        rest = dict()
+
+        rest["rho"] = measurements["np"] - measurements["nh"]
+        rest["N"]   = measurements["np"] + measurements["nh"]
+        rest["S3"]  = 0.5 * ( 1 - rest["N"])
+
+        return {**measurements, **rest}
+
+    except KeyError:
+        raise KeyError(f"Needed fields np, nh are not both available in {measurements.keys()}")
+
+    except ValueError:
+        raise ValueError("Particle and hole one-point measurements are incompatible shapes, {measurements['np'].shape} and {measurements['nh'].shape}, respectively.")
