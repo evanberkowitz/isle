b58d26b0e4f458a1832b336248b822dfbfde6e7d
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2020-03-20 16:01:32 +0100

    Remove unused imports

--- src/isle/meas/spinSpinCorrelator.py
+++ src/isle/meas/spinSpinCorrelator.py
@@ -397,9 +397,7 @@ Because we do not assume a bipartite graph, we implement Meng and Wessel's \f$S_
 from logging import getLogger
 
 import numpy as np
-import h5py as h5
 
-import isle
 from .measurement import Measurement
 from ..util import temporalRoller, signAlternator
 from ..h5io import createH5Group
