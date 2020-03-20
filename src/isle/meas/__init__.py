edf54b1363af8f6577f8ea78473c0228f888891d
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2020-03-20 15:23:00 +0100

    Import all measurements the same way

--- src/isle/meas/__init__.py
+++ src/isle/meas/__init__.py
@@ -10,10 +10,9 @@ from .action import Action  # (unused import) pylint: disable=W0611
 from .chiralCondensate import ChiralCondensate  # (unused import) pylint: disable=W0611
 from .collectWeights import CollectWeights  # (unused import) pylint: disable=W0611
 from .logdet import Logdet  # (unused import) pylint: disable=W0611
+from .onePointFunctions import OnePointFunctions  # (unused import) pylint: disable=W0611
 from .singleParticleCorrelator import SingleParticleCorrelator  # (unused import) pylint: disable=W0611
+from .spinSpinCorrelator import SpinSpinCorrelator  # (unused import) pylint: disable=W0611
 from .totalPhi import TotalPhi  # (unused import) pylint: disable=W0611
-from .polyakov import Polyakov
-from .determinantCorrelators import DeterminantCorrelators
-
-import isle.meas.onePointFunctions
-import isle.meas.spinSpinCorrelator
+from .polyakov import Polyakov  # (unused import) pylint: disable=W0611
+from .determinantCorrelators import DeterminantCorrelators  # (unused import) pylint: disable=W0611
