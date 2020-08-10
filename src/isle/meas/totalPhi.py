ed20828cf0f4931342f87e27b1e6a11bf8f88a2e
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2020-08-10 17:17:00 +0200

    Use TimeSeries in TotalPhi measurement

--- src/isle/meas/totalPhi.py
+++ src/isle/meas/totalPhi.py
@@ -3,10 +3,12 @@ r"""!\file
 Measurement of total phi and norm of phi.
 """
 
+from pathlib import Path
+
 import numpy as np
 
 from .measurement import Measurement
-from ..h5io import createH5Group
+
 
 class TotalPhi(Measurement):
     r"""!
@@ -17,13 +19,20 @@ class TotalPhi(Measurement):
     def __init__(self, savePath, configSlice=slice(None, None, None)):
         super().__init__(savePath, configSlice)
 
-        self.Phi = []
-        self.phiSq = []
+    def setup(self, memoryAllowance, expectedNConfigs, file):
+        if self.isSetUp():
+            raise RuntimeError("Cannot set up measurement, buffers are already set.")
+
+        self._allocateBuffers((("Phi", np.complex128, (), Path(self.savePath)/"totalPhi"),
+                               ("phiSquared", np.float64, (), Path(self.savePath)/"phiSquared")),
+                              memoryAllowance,
+                              expectedNConfigs,
+                              file)
 
     def __call__(self, stage, itr):
         """!Record the total phi and mean value of phi^2."""
-        self.Phi.append(np.sum(stage.phi))
-        self.phiSq.append(np.linalg.norm(stage.phi)**2 / len(stage.phi))
+        next(self.PhiIterator)[1][...] = np.sum(stage.phi)
+        next(self.phiSquaredIterator)[1][...] = np.linalg.norm(stage.phi)**2 / len(stage.phi)
 
     def save(self, h5group):
         r"""!
@@ -31,9 +40,10 @@ class TotalPhi(Measurement):
         \param base HDF5 group in which to store data.
         \param h5group Base HDF5 group. Data is stored in subgroup `h5group/self.savePath`.
         """
-        subGroup = createH5Group(h5group, self.savePath)
-        subGroup["totalPhi"] = self.Phi
-        subGroup["phiSquared"] = self.phiSq
+        raise NotImplementedError()
+        # subGroup = createH5Group(h5group, self.savePath)
+        # subGroup["totalPhi"] = self.Phi
+        # subGroup["phiSquared"] = self.phiSq
 
 
 def read(h5group):
