4907861ce030319a10b010661fed41fe918bd920
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2020-08-14 16:34:34 +0200

    Revert measure.py, was accidentally changed in 1616ee6

--- docs/examples/measure.py
+++ docs/examples/measure.py
@@ -22,11 +22,10 @@ def main():
     # Initialize Isle.
     # This sets up the command line interface, defines an argument parser for a measurement
     # command, and parses and returns arguments.
-    # args = isle.initialize("meas", prog="measure")
-
+    args = isle.initialize("meas", prog="measure")
 
     # Set up a measurement driver to run the measurements.
-    measState = isle.drivers.meas.init("hmc-example.out.h5", "meas.h5", True)
+    measState = isle.drivers.meas.init(args.infile, args.outfile, args.overwrite)
     # The driver has retrieved all previously stored parameters from the input file,
     params = measState.params
     # as well as the lattice including the number of time slices nt.
@@ -42,15 +41,15 @@ def main():
     # This object is a lower level interface for the Hubbard fermion action
     # needed by some measurements. The discretization (hopping) needs
     # to be selected manually.
-    # if params.hopping == isle.action.HFAHopping.DIA:
-    #     hfm = isle.HubbardFermiMatrixDia(kappaTilde, muTilde, params.sigmaKappa)
-    # else:
-    #     hfm = isle.HubbardFermiMatrixExp(kappaTilde, muTilde, params.sigmaKappa)
+    if params.hopping == isle.action.HFAHopping.DIA:
+        hfm = isle.HubbardFermiMatrixDia(kappaTilde, muTilde, params.sigmaKappa)
+    else:
+        hfm = isle.HubbardFermiMatrixExp(kappaTilde, muTilde, params.sigmaKappa)
 
     # Define measurements to run.
-    #allToAll = {s: isle.meas.propagator.AllToAll(hfm, s) for s in isle.Species.values()}
+    allToAll = {s: isle.meas.propagator.AllToAll(hfm, s) for s in isle.Species.values()}
 
-    #_, diagonalize = np.linalg.eigh(isle.Matrix(hfm.kappaTilde()))
+    _, diagonalize = np.linalg.eigh(isle.Matrix(hfm.kappaTilde()))
 
     #
     # The measurements are run on each configuration in the slice passed to 'configSlice'.
@@ -64,41 +63,41 @@ def main():
     # overwritten by accident.
     measurements = [
         # log(det(M)) where M is the fermion matrix
-        #isle.meas.Logdet(hfm, "logdet"),
+        isle.meas.Logdet(hfm, "logdet"),
         # \sum_i phi_i
         isle.meas.TotalPhi("field"),
         # collect all weights and store them in consolidated datasets instead of
         # spread out over many HDF5 groups
-        #isle.meas.CollectWeights("weights"),
+        isle.meas.CollectWeights("weights"),
         # polyakov loop
-        # isle.meas.Polyakov(params.basis, lat.nt(), "polyakov", configSlice=s_[::10]),
+        isle.meas.Polyakov(params.basis, lat.nt(), "polyakov", configSlice=s_[::10]),
         # # one-point functions
-        # isle.meas.OnePointFunctions(allToAll[isle.Species.PARTICLE],
-        #                             allToAll[isle.Species.HOLE],
-        #                             "correlation_functions/one_point",
-        #                             configSlice=s_[::10],
-        #                             transform=diagonalize),
-        # # single particle correlator for particles / spin up
-        # isle.meas.SingleParticleCorrelator(allToAll[isle.Species.PARTICLE],
-        #                                    "correlation_functions/single_particle",
-        #                                    configSlice=s_[::10],
-        #                                    transform=diagonalize),
-        # # single particle correlator for holes / spin down
-        # isle.meas.SingleParticleCorrelator(allToAll[isle.Species.HOLE],
-        #                                    "correlation_functions/single_hole",
-        #                                    configSlice=s_[::10],
-        #                                    transform=diagonalize),
-        # isle.meas.SpinSpinCorrelator(allToAll[isle.Species.PARTICLE],
-        #                              allToAll[isle.Species.HOLE],
-        #                              "correlation_functions/spin_spin",
-        #                              configSlice=s_[::10],
-        #                              transform=diagonalize,
-        #                              sigmaKappa=params.sigmaKappa),
-        # isle.meas.DeterminantCorrelators(allToAll[isle.Species.PARTICLE],
-        #                                  allToAll[isle.Species.HOLE],
-        #                                  "correlation_functions/det",
-        #                                  configSlice=s_[::10],
-        #)
+        isle.meas.OnePointFunctions(allToAll[isle.Species.PARTICLE],
+                                    allToAll[isle.Species.HOLE],
+                                    "correlation_functions/one_point",
+                                    configSlice=s_[::10],
+                                    transform=diagonalize),
+        # single particle correlator for particles / spin up
+        isle.meas.SingleParticleCorrelator(allToAll[isle.Species.PARTICLE],
+                                           "correlation_functions/single_particle",
+                                           configSlice=s_[::10],
+                                           transform=diagonalize),
+        # single particle correlator for holes / spin down
+        isle.meas.SingleParticleCorrelator(allToAll[isle.Species.HOLE],
+                                           "correlation_functions/single_hole",
+                                           configSlice=s_[::10],
+                                           transform=diagonalize),
+        isle.meas.SpinSpinCorrelator(allToAll[isle.Species.PARTICLE],
+                                     allToAll[isle.Species.HOLE],
+                                     "correlation_functions/spin_spin",
+                                     configSlice=s_[::10],
+                                     transform=diagonalize,
+                                     sigmaKappa=params.sigmaKappa),
+        isle.meas.DeterminantCorrelators(allToAll[isle.Species.PARTICLE],
+                                         allToAll[isle.Species.HOLE],
+                                         "correlation_functions/det",
+                                         configSlice=s_[::10],
+        )
     ]
 
     # Run the measurements on all configurations in the input file.
