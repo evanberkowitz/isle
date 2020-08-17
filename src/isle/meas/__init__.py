0b7f6bfaf2d9fad58e837efbca6a174cdf84da62
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2020-08-17 14:35:56 +0200

    Import AllToAll in meas/__init__

--- src/isle/meas/__init__.py
+++ src/isle/meas/__init__.py
@@ -5,13 +5,14 @@ General measurements that can be useful in various contexts.
 ## \defgroup meas Measurements
 # Perform measurements on configurations.
 
-from .measurement import Measurement  # (unused import) pylint: disable=W0611
 from .chiralCondensate import ChiralCondensate  # (unused import) pylint: disable=W0611
 from .collectWeights import CollectWeights  # (unused import) pylint: disable=W0611
+from .determinantCorrelators import DeterminantCorrelators  # (unused import) pylint: disable=W0611
 from .logdet import Logdet  # (unused import) pylint: disable=W0611
+from .measurement import Measurement  # (unused import) pylint: disable=W0611
 from .onePointFunctions import OnePointFunctions  # (unused import) pylint: disable=W0611
+from .polyakov import Polyakov  # (unused import) pylint: disable=W0611
+from .propagator import AllToAll  # (unused import) pylint: disable=W0611
 from .singleParticleCorrelator import SingleParticleCorrelator  # (unused import) pylint: disable=W0611
 from .spinSpinCorrelator import SpinSpinCorrelator  # (unused import) pylint: disable=W0611
 from .totalPhi import TotalPhi  # (unused import) pylint: disable=W0611
-from .polyakov import Polyakov  # (unused import) pylint: disable=W0611
-from .determinantCorrelators import DeterminantCorrelators  # (unused import) pylint: disable=W0611
