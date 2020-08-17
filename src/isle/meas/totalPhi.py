7e8c0f1cd293d93aa09f4a7eabc8a2567c3c82b0
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2020-08-17 12:18:23 +0200

    Remove read method for TotalPhi

--- src/isle/meas/totalPhi.py
+++ src/isle/meas/totalPhi.py
@@ -24,11 +24,3 @@ class TotalPhi(Measurement):
         """!Record the total phi and mean value of phi^2."""
         self.nextItem("Phi")[...] = np.sum(stage.phi)
         self.nextItem("phiSquared")[...] = np.linalg.norm(stage.phi)**2 / len(stage.phi)
-
-
-def read(h5group):
-    r"""!
-    Read Phi and phiSquared from a file.
-    \param h5group HDF5 group which contains the data of this measurement.
-    """
-    return h5group["totalPhi"][()], h5group["phiSquared"][()]
