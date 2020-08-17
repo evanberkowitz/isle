026fd5924002e6176baa063acc6c03332bc920c4
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2020-08-17 12:30:45 +0200

    Use buffers in Logdet

--- src/isle/meas/logdet.py
+++ src/isle/meas/logdet.py
@@ -4,9 +4,10 @@ Measurement of logdet.
 """
 
 import numpy as np
+from pentinsula.h5utils import open_or_pass_file
 
 import isle
-from .measurement import Measurement
+from .measurement import Measurement, BufferSpec
 
 class Logdet(Measurement):
     r"""!
@@ -17,51 +18,30 @@ class Logdet(Measurement):
     def __init__(self, hfm, savePath, configSlice=slice(None, None, None), alpha=1):
         assert alpha in [0, 1]
 
-        super().__init__(savePath, configSlice)
+        super().__init__(savePath,
+                         (BufferSpec("particles", (), np.complex128, "particles"),
+                          BufferSpec("holes", (), np.complex128, "holes")),
+                         configSlice)
 
         self.hfm = hfm
-        self.logdet = {isle.Species.PARTICLE: [],
-                       isle.Species.HOLE: []}
         self.alpha = alpha
 
     def __call__(self, stage, itr):
         """!Record logdet."""
         if self.alpha == 1:
-            for species, logdet in self.logdet.items():
-                logdet.append(isle.logdetM(self.hfm, stage.phi, species))
+            self.nextItem("particles")[...] = isle.logdetM(self.hfm, stage.phi,
+                                                           isle.Species.PARTICLE)
+            self.nextItem("holes")[...] = isle.logdetM(self.hfm, stage.phi,
+                                                       isle.Species.HOLE)
         else:
-            for species, logdet in self.logdet.items():
-                # use dense, slow numpy routine to get stable result
-                ld = np.linalg.slogdet(isle.Matrix(self.hfm.M(-1j*stage.phi, species)))
-                logdet.append(np.log(ld[0]) + ld[1])
+            # use dense, slow numpy routine to get stable result
+            ld = np.linalg.slogdet(isle.Matrix(self.hfm.M(-1j*stage.phi, isle.Species.PARTICLE)))
+            self.nextItem("particles")[...] = np.log(ld[0]) + ld[1]
+            ld = np.linalg.slogdet(isle.Matrix(self.hfm.M(-1j*stage.phi, isle.Species.HOLE)))
+            self.nextItem("holes")[...] = np.log(ld[0]) + ld[1]
 
-    def save(self, h5group):
-        r"""!
-        Write both the particle and hole logdet to a file.
-        \param h5group Base HDF5 group. Data is stored in subgroup `h5group/self.savePath`.
-        """
-        subGroup = isle.h5io.createH5Group(h5group, self.savePath)
-        subGroup["particles"] = self.logdet[isle.Species.PARTICLE]
-        subGroup["holes"] = self.logdet[isle.Species.HOLE]
-
-    def saveAll(self, h5group):
-        r"""!
-        Save results of measurement as well as relevant metadata.
-        \param h5group Base HDF5 group. Data is stored in subgroup `h5group/self.savePath`.
-        """
-
-        self.save(h5group)
-        # create the group here to make sure it really exists
-        subGroup = isle.h5io.createH5Group(h5group, self.savePath)
-        self.saveConfigSlice(subGroup)
-        subGroup.attrs["alpha"] = self.alpha
-
-
-def read(h5group):
-    r"""!
-    Read particle and hole logdet from a file.
-    \param h5group HDF5 group which contains the data of this measurement.
-    """
-    return {isle.Species.PARTICLE: h5group["particles"][()],
-            isle.Species.HOLE: h5group["holes"][()]}, \
-            h5group.attrs["alpha"] if "alpha" in h5group.attrs else None
+    def setup(self, memoryAllowance, expectedNConfigs, file, maxBufferSize=None):
+        res = super().setup(memoryAllowance, expectedNConfigs, file, maxBufferSize)
+        with open_or_pass_file(file, None, "a") as h5f:
+            h5f[self.savePath].attrs["alpha"] = self.alpha
+        return res
