765f6176babeb66a66a38a01f9361daf75c1c3c6
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-03-08 22:27:29 +0100

    cmake-format

--- cmake/Cache.cmake
+++ cmake/Cache.cmake
@@ -8,16 +8,13 @@ set(CACHE_OPTION
     CACHE STRING "Compiler cache to be used")
 set(CACHE_OPTION_VALUES "ccache" "sccache")
 set_property(CACHE CACHE_OPTION PROPERTY STRINGS ${CACHE_OPTION_VALUES})
-list(
-    FIND
-    CACHE_OPTION_VALUES
-    ${CACHE_OPTION}
-    CACHE_OPTION_INDEX)
+list(FIND CACHE_OPTION_VALUES ${CACHE_OPTION} CACHE_OPTION_INDEX)
 
 if(${CACHE_OPTION_INDEX} EQUAL -1)
   message(
-      STATUS
-      "Using custom compiler cache system: '${CACHE_OPTION}', explicitly supported entries are ${CACHE_OPTION_VALUES}")
+    STATUS
+      "Using custom compiler cache system: '${CACHE_OPTION}', explicitly supported entries are ${CACHE_OPTION_VALUES}"
+  )
 endif()
 
 find_program(CACHE_BINARY ${CACHE_OPTION})
