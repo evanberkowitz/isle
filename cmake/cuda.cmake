306dce5314d40a371bb10cd1c4bd79ef335018fb
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2021-03-19 15:34:26 +0100

    Added support for cuda::std::complex from libcudacxx (new dependency!)

--- cmake/cuda.cmake
+++ cmake/cuda.cmake
@@ -15,6 +15,22 @@ if (USE_CUDA)
   target_link_libraries(cudaAllocation PRIVATE CUDA::cudart)
   target_link_libraries(cudaAllocation PRIVATE project_options project_warnings)
 
+  message("-- Downloading external project - libcudacxx")
+
+  ExternalProject_Add( libcudacxx
+    PREFIX "${CMAKE_CURRENT_BINARY_DIR}"
+    DOWNLOAD_COMMAND git clone --depth 1 https://github.com/NVIDIA/libcudacxx.git
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+  )
+
+  ExternalProject_Get_Property(libcudacxx source_dir)
+  set(libcudacxx_INCLUDE_DIR "${source_dir}/include")
+
+  set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -I${libcudacxx_INCLUDE_DIR}")
+
   if (CUDA_PROFILE)
     target_link_libraries(cuda_toolkit INTERFACE CUDA::nvToolsExt)
     target_compile_definitions(cuda_toolkit INTERFACE -DENABLE_NVTX_PROFILE)
