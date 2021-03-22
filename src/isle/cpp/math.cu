919f046a889f93c2578ef6d12d1813bf2510aea3
Johann Ostmeyer <ostmeyer@hiskp.uni-bonn.de>
2021-03-22 17:34:27 +0100

    avoid implicit type cast

--- src/isle/cpp/math.cu
+++ src/isle/cpp/math.cu
@@ -2,22 +2,23 @@
 
 namespace isle{
 
-  CDMatrix mult_CDMatrix_wrapper(const CDMatrix &a, const CDMatrix &b, const std::size_t N) {
+  CDMatrix mult_CDMatrix_wrapper(const CDMatrix &a, const CDMatrix &b, const std::size_t dim) {
     cublasHandle_t handle;
     CHECK_CUBLAS_ERR(cublasCreate(&handle));
 
     const cuDoubleComplex alpha = make_cuDoubleComplex(1,0), beta = make_cuDoubleComplex(0,0);
+    const int N = static_cast<int>(dim);
 
     cuDoubleComplex * A;
     cuDoubleComplex * B;
     cuDoubleComplex * C;
 
-    CHECK_CU_ERR(cudaMalloc(&A, N*N*sizeof(cuDoubleComplex)));
-    CHECK_CU_ERR(cudaMalloc(&B, N*N*sizeof(cuDoubleComplex)));
-    CHECK_CU_ERR(cudaMalloc(&C, N*N*sizeof(cuDoubleComplex)));
+    CHECK_CU_ERR(cudaMalloc(&A, dim*dim*sizeof(cuDoubleComplex)));
+    CHECK_CU_ERR(cudaMalloc(&B, dim*dim*sizeof(cuDoubleComplex)));
+    CHECK_CU_ERR(cudaMalloc(&C, dim*dim*sizeof(cuDoubleComplex)));
 
-    CHECK_CU_ERR(cudaMemcpy(A, cast_cmpl(a.data()), N*N*sizeof(cuDoubleComplex), cudaMemcpyHostToDevice));
-    CHECK_CU_ERR(cudaMemcpy(B, cast_cmpl(b.data()), N*N*sizeof(cuDoubleComplex), cudaMemcpyHostToDevice));
+    CHECK_CU_ERR(cudaMemcpy(A, cast_cmpl(a.data()), dim*dim*sizeof(cuDoubleComplex), cudaMemcpyHostToDevice));
+    CHECK_CU_ERR(cudaMemcpy(B, cast_cmpl(b.data()), dim*dim*sizeof(cuDoubleComplex), cudaMemcpyHostToDevice));
 
     // 2nd and 3rd arguments: CUBLAS_OP_N means no transposition or conjugation 
     // 4-6,9,11,14-th arguments: matrix dimensions (all equal because matrices are square)
@@ -25,7 +26,7 @@ namespace isle{
     CHECK_CUBLAS_ERR(cublasZgemm3m(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, N, N, &alpha, A, N, B, N, &beta, C, N));
 
     CDMatrix res = a; // WARNING: This is very dirty and has to be removed later.
-    CHECK_CU_ERR(cudaMemcpy(res.data(), cast_cmpl(C), N*N*sizeof(cuDoubleComplex), cudaMemcpyDeviceToHost));
+    CHECK_CU_ERR(cudaMemcpy(res.data(), cast_cmpl(C), dim*dim*sizeof(cuDoubleComplex), cudaMemcpyDeviceToHost));
     cudaFree(A); cudaFree(B); cudaFree(C);
 
     CHECK_CUBLAS_ERR(cublasDestroy(handle));
