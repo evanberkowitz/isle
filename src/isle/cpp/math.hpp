bc584590c2835cd697d3c1291ca2415d1400174d
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2021-03-23 13:05:09 +0100

    removed ilogdet trace (compile error)

--- src/isle/cpp/math.hpp
+++ src/isle/cpp/math.hpp
@@ -173,12 +173,6 @@ namespace isle {
             throw std::invalid_argument("Invalid non-square matrix provided");
 #endif
 
-        #ifdef USE_CUDA
-
-        ilogdet_wrapper();
-
-        #else // USE_CUDA
-
         // pivot indices
         std::unique_ptr<int[]> ipiv = std::make_unique<int[]>(n);
         // perform LU decomposition (mat = PLU)
@@ -194,8 +188,6 @@ namespace isle {
             // log det of U (diagonal elements)
             res += std::log(std::complex<ET>{matrix(i, i)});
         }
-
-        #endif // USE_CUDA
 
         // combine log dets and project to (-pi, pi]
         return toFirstLogBranch(res + (negDetP ? std::complex<ET>{0, pi<ET>} : 0));
