c85c2801d8536723acb020c72470580c6f85f2a4
Thomas Luu <t.luu@fz-juelich.de>
2022-02-08 13:12:15 +0100

    worked on markdown

--- src/isle/meas/spatialCorrelators.py
+++ src/isle/meas/spatialCorrelators.py
@@ -1,64 +1,30 @@
 r"""!\file
 \ingroup meas
 
-# Single-Particle and -Hole operators
+# Spatial correlator operators 
 
-On each site we have four ladder operators, the particle destruction and creation
-operators, \f$a\f$ and \f$a^\dagger\f$, and the hole operators \f$b\f$, \f$b^\dagger\f$.
-
-Particles also have z-component of spin \f$-\frac{1}{2}\f$ because
-\f$[S^3_x, a_y^\dagger] = -\frac{1}{2} a_y^\dagger \delta_{xy}\f$
- and charge \f$+1\f$ because
-\f$[\rho_x, a_y^\dagger] = +1 a_y^\dagger \delta_{xy}\f$
-(see spinSpinCorrelator.py for more details).
-Similarly, holes have z-component of spin \f$-\frac{1}{2}\f$ (the same sign as particles!)
-but charge \f$-1\f$.
-
-We can therefore construct operators that create a definite charge but indefinite spin, and vice-versa,
+We calculate the operator
 \f{align}{
-    q^+_x &= \frac{a_x^\dagger + b_x}{\sqrt{2}}
-    &
-    q^-_x &= \frac{a_x + b_x^\dagger}{\sqrt{2}}
-    \\
-    s^+_x &= \frac{a_x + b_x}{\sqrt{2}}
-    &
-    s^-_x &= \frac{a_x^\dagger + b_x^\dagger}{\sqrt{2}}
+       & \langle a^{}_x(t)a^\dag_{k,\sigma}(0)\rangle
+       & \langle a^\dag_x(t)a^{}_{k,\sigma}(0)\rangle
 \f}
-so that \f$[\rho_x, q^\pm_y] = \pm 1           q^\pm_y \delta_{xy}\f$
-and     \f$[S^3_x,  s^\pm_y] = \pm \frac{1}{2} s^\pm_y \delta_{xy}\f$
-but these guys have indefinite (spin/charge, respectively) quantum number, and therefore mix
-under Hamiltonian evoluation.  Diagonalizing would reveal the particle/hole sectors
-already discussed, so we do not construct correlation functions with these operators,
-or any other linear combinations (there are plenty).
 
-# Correlation functions
+where 
 
-Now we can write correlation functions
 \f[
-    C^{ij}_{xy}(\tau) = \frac{1}{N_t} \sum_t \left\langle i_{x,t+\tau} j_{y,t} \right\rangle
+     a^\dag_{k,\sigma}=\sum_i P_i(k\sigma)a_i^\dag
 \f]
-and we suppress the time dependence for the time being.
 
-We can perform contractions,
-\f{align}{
-    C^{a a^\dagger}_{xy}(\tau)  &   = \left\langle a_{x} a_{y}^\dagger \right\rangle
-                                    = \left\langle P_{xy} \right\rangle
-                            &
-    C^{a^\dagger a}_{xy}(\tau)  &   = \left\langle a_x^\dagger a_y \right\rangle
-                                    = \left\langle \delta_{yx} - a_y a_x^\dagger \right\rangle
-                                    = \left\langle \delta_{yx} - P_{yx} \right\rangle
-                            \\
-    C^{b b^\dagger}_{xy}(\tau)  &   = \left\langle b_{x} b_{y}^\dagger \right\rangle
-                                    = \left\langle H_{xy} \right\rangle
-                            &
-    C^{b^\dagger b}_{xy}(\tau)  &   = \left\langle b_x^\dagger b_y \right\rangle
-                                    = \left\langle \delta_{yx} - b_y b_x^\dagger \right\rangle
-                                    = \left\langle \delta_{yx} - H_{yx} \right\rangle
-\f}
-and because of the identity \f$\{a_x,a_y^\dagger\} = \delta_{xy}\f$ one can show that
-in expectation value \f$C^{a a^\dagger}_{xy}(\tau) = C^{a^\dagger a}_{yx}(-\tau)\f$
-and likewise for holes.  Note that this is true in expectation value and not
-configuration-by-configuration.  Therefore it is worthwhile to perform both measurements.
+and \f$P_i(k\sigma)\f$ are projection operators to a particular \f$k,\sigma\f$ (ie. momentum,band).  
+Such projection operators are gotten, for example, from diagonalizing the connectivity matrix.
+
+In terms of the fermion matrix \f$M\f$ and the ensemble of configurations \f$\{\Phi\}\f$ this expression becomes
+\f[
+    C(x,t)= \frac{1}{N_{cfg}}\sum_{i\in\{\Phi\}}\sum_y M^{-1}[xt,y0;\phi_i]P_y(k\sigma)
+\f]
+
+One can also average over initial time sources (taking into account the anti-periodic BCs in time) to increase 
+statistics.  (the code does do this)
 
 """
 
@@ -72,7 +38,7 @@ from ..h5io import createH5Group, empty
 from .propagator import AllToAll
 
 
-class SingleParticleCorrelator(Measurement):
+class SpatialCorrelator(Measurement):
     r"""!
     \ingroup meas
     Tabulate single-particle correlator.
